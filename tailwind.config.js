6c2cc72c31738d0bb1b818bc8246dadf719dbe72
Jan Langheimer <langheimer@gmail.com>
2021-05-25 13:28:08 +0200

    add tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   mode: 'jit',
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
+    colors: {
+      ...colors,
+    },
     extend: {},
   },
   plugins: [],
