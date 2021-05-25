58c621b9518537a545c97b8a1323ce52fc172f5d
Jan Langheimer <langheimer@gmail.com>
2021-05-25 12:31:15 +0200

    enable tailwind production purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: 'jit',
-  purge: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
