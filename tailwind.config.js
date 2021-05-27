1672ece983d704679e4818056e6369998feb241d
Jan Langheimer <langheimer@gmail.com>
2021-05-27 16:49:16 +0200

    add tailwind outline config

--- tailwind.config.js
+++ tailwind.config.js
@@ -625,6 +625,12 @@ module.exports = {
       95: '0.95',
       100: '1',
     },
+
+    outline: {
+      none: ['2px solid transparent', '2px'],
+      white: ['2px dotted white', '2px'],
+      black: ['2px dotted black', '2px'],
+    },
   },
   plugins: [],
 };
