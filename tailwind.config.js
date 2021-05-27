d57ccb86c2a67c468a8884d6a8fe2b3010971dc2
Jan Langheimer <langheimer@gmail.com>
2021-05-27 16:38:26 +0200

    add tailwind blur config

--- tailwind.config.js
+++ tailwind.config.js
@@ -106,6 +106,17 @@ module.exports = {
       cover: 'cover',
       contain: 'contain',
     },
+
+    blur: {
+      0: '0',
+      sm: '4px',
+      DEFAULT: '8px',
+      md: '12px',
+      lg: '16px',
+      xl: '24px',
+      '2xl': '40px',
+      '3xl': '64px',
+    },
   },
   plugins: [],
 };
