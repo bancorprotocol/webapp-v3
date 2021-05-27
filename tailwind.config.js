19c6f6e590ab19c4fd053fd4a838435a0bb4ce79
Jan Langheimer <langheimer@gmail.com>
2021-05-27 16:39:02 +0200

    add tailwind border config

--- tailwind.config.js
+++ tailwind.config.js
@@ -131,6 +131,30 @@ module.exports = {
       150: '1.5',
       200: '2',
     },
+
+    borderColor: (theme) => ({
+      ...theme('colors'),
+      DEFAULT: theme('colors.gray.200', 'currentColor'),
+    }),
+    borderOpacity: (theme) => theme('opacity'),
+    borderRadius: {
+      none: '0px',
+      sm: '0.125rem',
+      DEFAULT: '0.25rem',
+      md: '0.375rem',
+      lg: '0.5rem',
+      xl: '0.75rem',
+      '2xl': '1rem',
+      '3xl': '1.5rem',
+      full: '9999px',
+    },
+    borderWidth: {
+      DEFAULT: '1px',
+      0: '0px',
+      2: '2px',
+      4: '4px',
+      8: '8px',
+    },
   },
   plugins: [],
 };
