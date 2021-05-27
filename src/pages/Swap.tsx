466fc1a68e34a3d88c147a6025339edae8166c73
Jan Langheimer <langheimer@gmail.com>
2021-05-27 17:41:43 +0200

    add button samples

--- src/pages/Swap.tsx
+++ src/pages/Swap.tsx
@@ -1,8 +1,17 @@
+import { Link } from 'react-router-dom';
+import { BaseModal } from '../components/base/BaseModal';
+
 export const Swap = () => {
   return (
+    <div className="space-y-5">
+      <h1>Swap</h1>
+      <h2>hi</h2>
       <div>
-        <h1>Swap</h1>
-        <h2>hi</h2>
+        <BaseModal />
       </div>
+      <Link to="/buttons">
+        <button className="btn-pink">Button Samples</button>
+      </Link>
+    </div>
   );
 };
