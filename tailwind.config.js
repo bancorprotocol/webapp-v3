874196ad17a71b867910855706ae1a843e9d64c6
Jan Langheimer <langheimer@gmail.com>
2021-05-27 16:36:57 +0200

    add tailwind base color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
+    colors: {
+      ...colors
+    },
+
     screens: {
       sm: '640px',
       md: '768px',
