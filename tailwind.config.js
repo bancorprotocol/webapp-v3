54b1b996078969010ce9a6bae980f2849ebcd07c
Jan Langheimer <langheimer@gmail.com>
2021-05-27 16:32:29 +0200

    add tailwind screens config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,13 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    colors: {
-      ...colors,
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
     },
-    extend: {},
   },
   plugins: [],
 }
