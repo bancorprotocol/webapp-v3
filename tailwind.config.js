f518080637b72c8f53e3f87cd3ed298d59118554
Jan Langheimer <langheimer@gmail.com>
2021-05-27 16:47:39 +0200

    add tailwind maxWidth config

--- tailwind.config.js
+++ tailwind.config.js
@@ -485,6 +485,27 @@ module.exports = {
       screen: '100vh',
     }),
 
+    maxWidth: (theme, { breakpoints }) => ({
+      none: 'none',
+      0: '0rem',
+      xs: '20rem',
+      sm: '24rem',
+      md: '28rem',
+      lg: '32rem',
+      xl: '36rem',
+      '2xl': '42rem',
+      '3xl': '48rem',
+      '4xl': '56rem',
+      '5xl': '64rem',
+      '6xl': '72rem',
+      '7xl': '80rem',
+      full: '100%',
+      min: 'min-content',
+      max: 'max-content',
+      prose: '65ch',
+      ...breakpoints(theme('screens')),
+    }),
+
     inset: (theme, { negative }) => ({
       auto: 'auto',
       ...theme('spacing'),
