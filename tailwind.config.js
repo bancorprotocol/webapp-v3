80af3b946472f8e9d86dca34d5cac110b2b89d1d
Jan Langheimer <langheimer@gmail.com>
2021-05-27 16:48:25 +0200

    add tailwind objectPosition config

--- tailwind.config.js
+++ tailwind.config.js
@@ -577,6 +577,18 @@ module.exports = {
       ...theme('spacing'),
       ...negative(theme('spacing')),
     }),
+
+    objectPosition: {
+      bottom: 'bottom',
+      center: 'center',
+      left: 'left',
+      'left-bottom': 'left bottom',
+      'left-top': 'left top',
+      right: 'right',
+      'right-bottom': 'right bottom',
+      'right-top': 'right top',
+      top: 'top',
+    },
   },
   plugins: [],
 };
