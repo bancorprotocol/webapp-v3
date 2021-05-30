c99ce834e27c3910efe10e95560a357e4a916829
Ran Cohen <ran@bancor.network>
2021-05-30 18:04:05 +0300

    Added inputfield

---
+++ src/components/InputField.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+export const InputField = ({
+  placeholder,
+  disabled,
+  format,
+}: {
+  placeholder?: string;
+  disabled?: boolean;
+  format?: boolean;
+}) => {
+  const [input, setInput] = useState('');
+
+  const handleChange = (text: string) => {
+    setInput(
+      format
+        ? text
+            .replace(/[^\d\.]/g, '')
+            .replace(/\./, 'x')
+            .replace(/\./g, '')
+            .replace(/x/, '.')
+        : text
+    );
+  };
+
+  return (
+    <>
+      <input
+        type="text"
+        placeholder={placeholder}
+        className="focus:outline-none focus:border-blue-300 border-2 border-gray-600 rounded px-4 h-10"
+        onChange={(event) => handleChange(event.target.value)}
+      />
+    </>
+  );
+};
--- src/pages/Swap.tsx
+++ src/pages/Swap.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { BaseModal } from 'components/base/BaseModal';
 import { Dropdown } from 'components/Dropdown';
+import { InputField } from 'components/InputField';
 
 export const Swap = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -24,25 +25,24 @@ export const Swap = () => {
       <Link to="/buttons">
         <button className="btn-pink">Button Samples</button>
       </Link>
-      <div className="h-20">
-        <Dropdown
-          title={'Dropdown'}
-          items={[
-            { id: '1', title: 'item1' },
-            { id: '2', title: 'item2' },
-            { id: '3', title: 'item3' },
-            { id: '4', title: 'item4' },
-            { id: '5', title: 'item5' },
-            { id: '6', title: 'item6' },
-            { id: '7', title: 'item7' },
-            { id: '8', title: 'item8' },
-            { id: '9', title: 'item9' },
-            { id: '10', title: 'item10' },
-            { id: '11', title: 'item11' },
-            { id: '12', title: 'item12' },
-          ]}
-        />
-      </div>
+      <InputField format placeholder="Input field" />
+      <Dropdown
+        title={'Dropdown'}
+        items={[
+          { id: '1', title: 'item1' },
+          { id: '2', title: 'item2' },
+          { id: '3', title: 'item3' },
+          { id: '4', title: 'item4' },
+          { id: '5', title: 'item5' },
+          { id: '6', title: 'item6' },
+          { id: '7', title: 'item7' },
+          { id: '8', title: 'item8' },
+          { id: '9', title: 'item9' },
+          { id: '10', title: 'item10' },
+          { id: '11', title: 'item11' },
+          { id: '12', title: 'item12' },
+        ]}
+      />
     </div>
   );
 };
