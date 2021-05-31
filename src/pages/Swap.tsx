cee01dcfb3bcf561f06c69e3dafc09c2d2428175
Jan Langheimer <langheimer@gmail.com>
2021-05-31 14:25:38 +0200

    test build action

--- src/pages/Swap.tsx
+++ src/pages/Swap.tsx
@@ -41,7 +41,7 @@ export const Swap = () => {
               className="inline-flex justify-center px-4 py-2 font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
               onClick={() => setIsOpen(false)}
             >
-              Confirm!
+              Close
             </button>
           </div>
         </BaseModal>
