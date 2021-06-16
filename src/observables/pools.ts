45fe5ae35e039389eca400351de15d409f772514
John Williamson <johnjwilliamson@live.com>
2021-06-16 21:58:07 +1000

    half

--- src/observables/pools.ts
+++ src/observables/pools.ts
@@ -1,6 +1,6 @@
-import { getWelcomeData, WelcomeData } from 'api/bancor';
-import { isEqual, zip } from 'lodash';
-import { combineLatest } from 'rxjs';
+import { getWelcomeData, Pool, Token, WelcomeData } from 'api/bancor';
+import { isEqual, partition, uniq, uniqBy, zip } from 'lodash';
+import { combineLatest, Subject } from 'rxjs';
 import {
   distinctUntilChanged,
   map,
@@ -8,6 +8,8 @@ import {
   share,
   shareReplay,
   startWith,
+  switchMap,
+  withLatestFrom,
 } from 'rxjs/operators';
 import { ConverterAndAnchor } from 'web3/types';
 import { bancorConverterRegistry$ } from './contracts';
@@ -20,7 +22,12 @@ import {
 } from 'web3/contracts/converterRegistry/wrapper';
 import { web3 } from 'web3/contracts';
 import { toChecksumAddress } from 'web3-utils';
-import { updateArray } from 'helpers';
+import {
+  findNewPath,
+  findOrThrow,
+  mapIgnoreThrown,
+  updateArray,
+} from 'helpers';
 
 const zipAnchorAndConverters = (
   anchorAddresses: string[],
@@ -112,3 +119,176 @@ export const apiTokens$ = apiData$.pipe(
   distinctUntilChanged<WelcomeData['tokens']>(isEqual),
   share()
 );
+
+interface SwapOptions {
+  fromId: string;
+  toId: string;
+  decAmount: string;
+}
+
+interface MinimalPool {
+  anchorAddress: string;
+  contract: string;
+  reserves: string[];
+}
+
+const toMinimal = (pool: Pool): MinimalPool => ({
+  anchorAddress: pool.pool_dlt_id,
+  contract: pool.converter_dlt_id,
+  reserves: pool.reserves.map((reserve) => reserve.address),
+});
+
+const swapReceiver$ = new Subject<SwapOptions>();
+
+const sortByLiqDepth = (a: Pool, b: Pool) =>
+  Number(b.liquidity.usd) - Number(a.liquidity.usd);
+
+const dropDuplicateReservesByHigherLiquidity = (pools: Pool[]) => {
+  const sortedByLiquidityDepth = pools.sort(sortByLiqDepth);
+  const uniquePools = uniqBy(sortedByLiquidityDepth, 'pool_dlt_id');
+
+  return uniquePools;
+};
+
+const poolHasBalances = (pool: Pool) =>
+  pool.reserves.every((reserve) => reserve.balance !== '0');
+
+const filterTradeWorthyPools = (pools: Pool[]) => {
+  const poolsWithBalances = pools.filter(poolHasBalances);
+  const removedDuplicates =
+    dropDuplicateReservesByHigherLiquidity(poolsWithBalances);
+  return removedDuplicates;
+};
+
+const minimalPoolReserves = (pool: MinimalPool): [string, string] => {
+  if (pool.reserves.length !== 2)
+    throw new Error('Was expecting a pool of 2 reserves');
+  return pool.reserves as [string, string];
+};
+
+type TradePath = MinimalPool[];
+
+const possiblePaths = async (
+  from: string,
+  to: string,
+  pools: MinimalPool[]
+): Promise<TradePath[]> => {
+  const singlePool = pools.find((pool) =>
+    [from, to].every((tradedToken) =>
+      pool.reserves.some((reserve) => tradedToken === reserve)
+    )
+  );
+  if (singlePool) {
+    return [[singlePool]];
+  }
+
+  const [validStartingPools, remainingPools] = partition(pools, (pool) =>
+    pool.reserves.some((reserve) => reserve === from)
+  );
+  const isValidTerminatingPool = pools.some((relay) =>
+    relay.reserves.some((reserve) => reserve === to)
+  );
+  const areSufficientPools =
+    validStartingPools.length > 0 && isValidTerminatingPool;
+
+  if (!areSufficientPools)
+    throw new Error(`No pools found containing both the from and to target`);
+
+  const moreThanOneStartingPool = validStartingPools.length > 1;
+  if (moreThanOneStartingPool) {
+    const results = await mapIgnoreThrown(
+      validStartingPools,
+      async (validStartingPool) => {
+        const isolatedPools = [validStartingPool, ...remainingPools];
+        const poolPath = await findNewPath(
+          from,
+          to,
+          isolatedPools,
+          minimalPoolReserves
+        );
+        return poolPath.hops.flatMap((hop) => hop[0]);
+      }
+    );
+    return results;
+  } else {
+    const res = await findNewPath(from, to, pools, minimalPoolReserves);
+    return [res.hops.flatMap((hop) => hop[0])];
+  }
+};
+
+const hasAnchor = (anchor: string) => (pool: Pool) =>
+  pool.pool_dlt_id === anchor;
+
+const hasTokenId = (tokenAddress: string) => (token: Token) =>
+  token.dlt_id === tokenAddress;
+
+const sortPathByBiggestStartingPool = (paths: TradePath[], pools: Pool[]) => {
+  const allAnchors = paths.flat(2);
+  const uniqueAnchors = uniq(allAnchors);
+  const allPoolsFound = uniqueAnchors.every((minimalPool) =>
+    pools.some(
+      (pool) =>
+        pool.pool_dlt_id === minimalPool.anchorAddress &&
+        pool.converter_dlt_id === minimalPool.contract
+    )
+  );
+  if (!allPoolsFound)
+    throw new Error('Not all paths anchors were found in pools array');
+  return paths.sort((a, b) => {
+    const startingA = a[0];
+    const startingB = b[0];
+    const startingAPool = pools.find(hasAnchor(startingA.anchorAddress))!;
+    const startingBPool = pools.find(hasAnchor(startingB.anchorAddress))!;
+    return sortByLiqDepth(startingAPool, startingBPool);
+  });
+};
+
+const tradeAndPath$ = swapReceiver$.pipe(
+  withLatestFrom(pools$),
+  switchMapIgnoreThrow(async ([trade, pools]) => {
+    const winningPools = filterTradeWorthyPools(pools);
+    const minimalPools = winningPools.map(toMinimal);
+
+    const potentialPaths = await possiblePaths(
+      trade.fromId,
+      trade.toId,
+      minimalPools
+    );
+    const [biggestStartingPath] = sortPathByBiggestStartingPool(
+      potentialPaths,
+      winningPools
+    );
+    return { path: biggestStartingPath, trade };
+  }),
+  shareReplay(1)
+);
+
+const rate$ = tradeAndPath$.pipe(
+  withLatestFrom(apiTokens$),
+  switchMapIgnoreThrow(async ([trade, tokens]) => {
+    const fromToken = findOrThrow(tokens, hasTokenId(trade.trade.fromId));
+    const toToken = findOrThrow(tokens, hasTokenId(trade.trade.toId));
+
+    const fromWei = expandToken;
+    trade.path;
+  })
+);
+
+const swapTx$ = tradeAndPath$.pipe(
+  withLatestFrom(apiTokens$),
+  switchMap(async ([{ path, trade }, tokens]) => {
+    const fromWei = findOrThrow(
+      tokens,
+      (token) => token.dlt_id === trade.fromId,
+      'failed finding from token in tokens observable'
+    );
+
+    // insert error handling here
+  })
+);
+
+export const tx = {
+  swap: function (fromId: string, toId: string, decAmount: string) {
+    swapReceiver$.next({ fromId, toId, decAmount });
+  },
+};
