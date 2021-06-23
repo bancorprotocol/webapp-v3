9827d9b4287fe28f014d083e3967496de5549dd0
Jan Langheimer <langheimer@gmail.com>
2021-06-23 11:40:04 +0200

    add notification animation

--- src/elements/layoutHeader/notificationsMenu/NotificationsMenu.tsx
+++ src/elements/layoutHeader/notificationsMenu/NotificationsMenu.tsx
@@ -9,7 +9,7 @@ import {
 } from 'redux/notification/notification';
 import { NotificationContent } from 'elements/notifications/NotificationContent';
 import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 
 export const NotificationsMenu = () => {
   const dispatch = useDispatch();
@@ -17,6 +17,8 @@ export const NotificationsMenu = () => {
   const notifications = useAppSelector<Notification[]>(
     (state) => state.notification.notifications
   );
+
+  const sorted = useMemo(() => [...notifications].reverse(), [notifications]);
 
   useEffect(() => {
     const restored = localStorage.getItem('notifications');
@@ -37,29 +39,31 @@ export const NotificationsMenu = () => {
         <Popover.Panel className="dropdown-menu">
           <div className="dropdown-bubble" />
 
-          <div className="dropdown-header flex justify-between">
-            <h3 className="text-16 font-semibold">Notifications</h3>
-            <button
-              onClick={() => dispatch(setNotifications([]))}
-              className="text-12 underline"
-            >
-              clear
-            </button>
-          </div>
+          <div className="-mr-18 pr-18 max-h-[400px] overflow-auto">
+            <div className="dropdown-header flex justify-between">
+              <h3 className="text-16 font-semibold">Notifications</h3>
+              <button
+                onClick={() => dispatch(setNotifications([]))}
+                className="text-12 underline"
+              >
+                clear
+              </button>
+            </div>
 
-          {notifications.map((notification, index) => {
-            return (
-              <div key={index}>
-                <NotificationContent
-                  data={notification}
-                  onRemove={(id: string) => dispatch(removeNotification(id))}
-                />
-                {notifications.length > index + 1 && (
-                  <hr className="my-10 border-grey-3 border-opacity-50" />
-                )}
-              </div>
-            );
-          })}
+            {sorted.map((notification, index) => {
+              return (
+                <div key={index}>
+                  <NotificationContent
+                    data={notification}
+                    onRemove={(id: string) => dispatch(removeNotification(id))}
+                  />
+                  {notifications.length > index + 1 && (
+                    <hr className="my-10 border-grey-3 border-opacity-50" />
+                  )}
+                </div>
+              );
+            })}
+          </div>
         </Popover.Panel>
       </DropdownTransition>
     </Popover>
