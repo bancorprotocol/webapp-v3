9fdc27402f3e4757182b18797acd55b6a23564dd
Jan Langheimer <langheimer@gmail.com>
2021-06-28 16:21:09 +0200

    add price impact calculation with debug logs

--- src/services/web3/swap/methods.ts
+++ src/services/web3/swap/methods.ts
@@ -1,6 +1,7 @@
 import {
   conversionPath,
   getRateByPath,
+  getReturnByPath,
 } from 'services/web3/contracts/network/wrapper';
 import { web3 } from 'services/web3/contracts';
 import { bancorNetwork$ } from 'services/observables/contracts';
@@ -31,4 +32,72 @@ export const getRate = async (
   });
 
   return shrinkToken(toAmountWei, toToken.decimals);
+};
+
+export const getPriceImpact = async (
+  fromToken: TokenListItem,
+  toToken: TokenListItem,
+  amount: string
+) => {
+  const networkContractAddress = await bancorNetwork$.pipe(take(1)).toPromise();
+
+  console.log('--- INPUT ---');
+  console.log('fromToken: ', fromToken.address);
+  console.log('toToken: ', toToken.address);
+  console.log('inputAmountWei: ', expandToken(amount, fromToken.decimals));
+  console.log('--- INPUT END ---');
+
+  const path = await conversionPath({
+    from: fromToken.address,
+    to: toToken.address,
+    networkContractAddress,
+    web3,
+  });
+
+  console.log('--- CONVERSION PATH ---');
+  console.log(path);
+  console.log('--- CONVERSION PATH END ---');
+
+  const result = await getReturnByPath({
+    networkContractAddress,
+    amount: expandToken(amount, fromToken.decimals),
+    path,
+    web3,
+  });
+
+  console.log('--- GETRETURNPATH METHOD ---');
+  console.log('outputAmountWei: ', result['0']);
+  console.log('--- GETRETURNPATH METHOD END ---');
+
+  const pathInverted = await conversionPath({
+    from: toToken.address,
+    to: fromToken.address,
+    networkContractAddress,
+    web3,
+  });
+
+  console.log('--- INVERTED CONVERSION PATH ---');
+  console.log(pathInverted);
+  console.log('--- INVERTED CONVERSION PATH END ---');
+
+  const resultInverted = await getReturnByPath({
+    networkContractAddress,
+    amount: result['0'],
+    path: pathInverted,
+    web3,
+  });
+
+  console.log('--- INVERTED GETRETURNPATH METHOD ---');
+  console.log('invertedOutputAmountWei: ', resultInverted['0']);
+  console.log('--- INVERTED GETRETURNPATH METHOD END ---');
+
+  const output = Number(resultInverted['0']);
+  const input = Number(expandToken(amount, fromToken.decimals));
+
+  const priceImpact = ((1 - output / input) * 100) / 2;
+  console.log('--- RESULT PRICE IMPACT ---');
+  console.log('priceImpact: ', priceImpact);
+  console.log('--- RESULT PRICE IMPACT END ---');
+
+  return priceImpact;
 };
