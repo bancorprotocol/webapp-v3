d1e5cf50be70727354da006b173fb5f5b5ef10d7
Jan Langheimer <langheimer@gmail.com>
2021-08-03 18:20:01 +0200

    cleanup

--- src/elements/tokens/TopTokens.tsx
+++ src/elements/tokens/TopTokens.tsx
@@ -1,22 +1,22 @@
 import { Ticker } from 'components/Ticker';
-import { useAppSelector } from '../../redux';
+import { useAppSelector } from 'redux/index';
 import { Token } from 'services/observables/tokens';
-import { prettifyNumber } from '../../utils/helperFunctions';
+import { prettifyNumber } from 'utils/helperFunctions';
 
 export const TopTokens = () => {
   const tokens = useAppSelector<Token[]>((state) => state.bancor.tokens);
   const topTokens = tokens.slice(0, 20);
 
   return (
-    <section className="bg-white dark:bg-blue-4 md:rounded-20 pt-20 pb-10 shadow-widget">
-      <h2 className="font-semibold ml-20">Top Tokens</h2>
-      <hr className="my-14 mx-20 border-t-2 border-grey-2 dark:border-blue-1" />
+    <section className="content-section pt-20 pb-10">
+      <h2 className="ml-20">Top Tokens</h2>
+      <hr className="content-separator my-14 mx-20" />
       <Ticker id="top-tokens">
         <div className="flex space-x-16 mt-10">
           {topTokens.map((token) => (
             <div
               key={token.address}
-              className="flex items-center justify-center min-w-[150px] h-[75px] rounded-[6px] bg-blue-0 dark:bg-blue-2 shadow-widget dark:shadow-none"
+              className="flex items-center justify-center min-w-[150px] h-[75px] rounded-[6px] bg-blue-0 dark:bg-blue-2 shadow-content dark:shadow-none"
             >
               <img
                 src={token.logoURI.replace('thumb', 'small')}
--- tailwind.config.js
+++ tailwind.config.js
@@ -252,6 +252,7 @@ module.exports = {
         '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
       widget: '0px 2px 6px rgba(15, 49, 105, 0.15);',
       header: '0px 2px 6px rgba(15, 49, 105, 0.15)',
+      content: '0px 2px 6px rgba(15, 49, 105, 0.15)',
       md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
       lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
       xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
