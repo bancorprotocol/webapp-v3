dc62da1aeaa217e577ee61adac3c6d59e98ec669
Jan Langheimer <langheimer@gmail.com>
2021-08-04 13:31:26 +0200

    add line chart component

---
+++ src/components/charts/LineChartSimple.tsx
@@ -0,0 +1,84 @@
+import { createRef, useCallback, useEffect } from 'react';
+import {
+  ChartOptions,
+  createChart,
+  DeepPartial,
+  LineData,
+  LineStyleOptions,
+  SeriesOptionsCommon,
+} from 'lightweight-charts';
+
+const seriesOptDefault: DeepPartial<LineStyleOptions & SeriesOptionsCommon> = {
+  lineWidth: 1,
+  crosshairMarkerVisible: false,
+  priceLineVisible: false,
+};
+
+const chartOptDefault: DeepPartial<ChartOptions> = {
+  layout: {
+    backgroundColor: 'transparent',
+  },
+  crosshair: {
+    vertLine: {
+      visible: false,
+    },
+    horzLine: {
+      visible: false,
+    },
+  },
+  leftPriceScale: {
+    visible: false,
+    borderVisible: false,
+  },
+  rightPriceScale: {
+    visible: false,
+    borderVisible: false,
+  },
+  timeScale: {
+    visible: false,
+    borderVisible: false,
+  },
+  grid: {
+    horzLines: {
+      visible: false,
+    },
+    vertLines: {
+      visible: false,
+    },
+  },
+  handleScroll: false,
+  handleScale: false,
+};
+
+interface LineChartSimpleProps {
+  data: LineData[];
+  seriesOptions?: DeepPartial<LineStyleOptions & SeriesOptionsCommon>;
+  chartOptions?: DeepPartial<ChartOptions>;
+  size?: { width: number; height: number };
+}
+
+export const LineChartSimple = ({
+  data,
+  seriesOptions = seriesOptDefault,
+  chartOptions = chartOptDefault,
+  size = { width: 160, height: 31 },
+}: LineChartSimpleProps) => {
+  const chartDiv = createRef<HTMLDivElement>();
+
+  const initChart = useCallback(() => {
+    if (!chartDiv.current) return;
+
+    const chart = createChart(chartDiv.current, size);
+    chart.applyOptions(chartOptions);
+    chart.timeScale().fitContent();
+
+    const lineSeries = chart.addLineSeries(seriesOptions);
+    lineSeries.setData(data);
+  }, [chartDiv, data, seriesOptions, chartOptions, size]);
+
+  useEffect(() => {
+    initChart();
+  }, [initChart]);
+
+  return <div ref={chartDiv}></div>;
+};
