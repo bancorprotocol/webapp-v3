9ae0f98c1b8a6d5be5df33a62df016af049c9279
Jan Langheimer <langheimer@gmail.com>
2021-08-06 17:58:35 +0200

    add top tokens loading animation

--- src/elements/tokens/TopTokens.tsx
+++ src/elements/tokens/TopTokens.tsx
@@ -9,27 +9,31 @@ export const TopTokens = () => {
 
   return (
     <section className="content-section pt-20 pb-10">
-      <h2 className="ml-20">Top Tokens</h2>
+      <h2 className="ml-20">Top Movers</h2>
       <hr className="content-separator my-14 mx-20" />
       <Ticker id="top-tokens">
         <div className="flex space-x-16 mt-10">
-          {topTokens.map((token) => (
-            <div
-              key={token.address}
-              className="flex items-center justify-center min-w-[150px] h-[75px] rounded-[6px] bg-blue-0 dark:bg-blue-2 shadow-content dark:shadow-none"
-            >
-              <img
-                src={token.logoURI.replace('thumb', 'small')}
-                alt="Token Logo"
-                className="w-50 h-50 rounded-full"
-              />
-              <div className="ml-10 text-12 dark:text-grey-3">
-                <div className="font-medium">{token.symbol}</div>
-                <div>{prettifyNumber(token.usdPrice ?? 0, true)}</div>
-                <div className="font-bold text-success">+102.75%</div>
-              </div>
-            </div>
-          ))}
+          {tokens.length
+            ? topTokens.map((token) => (
+                <div
+                  key={token.address}
+                  className="flex items-center justify-center min-w-[150px] h-[75px] rounded-[6px] bg-blue-0 dark:bg-blue-2 shadow-content dark:shadow-none"
+                >
+                  <img
+                    src={token.logoURI.replace('thumb', 'small')}
+                    alt="Token Logo"
+                    className="w-50 h-50 rounded-full"
+                  />
+                  <div className="ml-10 text-12 dark:text-grey-3">
+                    <div className="font-medium">{token.symbol}</div>
+                    <div>{prettifyNumber(token.usdPrice ?? 0, true)}</div>
+                    <div className="font-bold text-success">+102.75%</div>
+                  </div>
+                </div>
+              ))
+            : [...Array(20)].map(() => (
+                <div className="rounded-[6px] bg-blue-0 dark:bg-blue-2 shadow-content dark:shadow-none min-w-[150px] h-[75px] animate-pulse"></div>
+              ))}
         </div>
       </Ticker>
     </section>
