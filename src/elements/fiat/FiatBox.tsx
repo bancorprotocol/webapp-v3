4f81e5ddb9490c8e4354aa07cc4ea380c7b292a9
Ran Cohen <ran@bancor.network>
2021-08-09 17:12:53 +0300

    added fiat box and one provider

---
+++ src/elements/fiat/FiatBox.tsx
@@ -0,0 +1,92 @@
+import { Image } from 'components/image/Image';
+import { ReactComponent as IconCheck } from 'assets/icons/check.svg';
+
+export enum Operations {
+  fiatIn,
+  fiatOut,
+}
+
+export enum DepositMethods {
+  wire = 'Wire Transfer',
+  creditCard = 'Credit Card',
+  debitCard = 'Debit Card',
+  samsumgPay = 'Samsumg Pay',
+  googlePay = 'Google Pay',
+  applePay = 'Apple Pay',
+}
+
+export interface Fiat {
+  name: string;
+  text: string;
+  logo: string;
+  operations: Operations[];
+  depositMethods: DepositMethods[];
+  buyUrl: string;
+  sellUrl: string;
+}
+
+export const FiatBox = ({ fiat }: { fiat: Fiat }) => {
+  const fiatIn = fiat.operations.includes(Operations.fiatIn);
+  const fiatOut = fiat.operations.includes(Operations.fiatOut);
+
+  return (
+    <div className="flex flex-col items-between bg-white dark:bg-blue-4 w-[550px] [h-360px] p-30 rounded-20 text-grey-4 dark:text-grey-0">
+      <Image
+        className="h-40 w-140 self-start text-grey-4 dark:text-grey-0"
+        src={fiat.logo}
+        alt={fiat.name}
+      />
+      <div className="flex items-center my-20 font-medium">
+        Available Operations
+        {fiatIn && (
+          <div className="text-10 text-white font-semibold bg-success-500 rounded-10 px-15 py-2 ml-10">
+            Fiat in
+          </div>
+        )}
+        {fiatOut && (
+          <div className="text-10 text-white font-semibold bg-primary-light rounded-10 px-15 py-2 ml-10">
+            Fiat Out
+          </div>
+        )}
+      </div>
+      {fiat.text}
+      <div className="flex justify-between items-center w-[288px] my-20">
+        {fiatIn && (
+          <a
+            className="btn-primary rounded w-full w-[135px] h-[40px]"
+            href={fiat.buyUrl}
+            rel="noreferrer"
+            target="_blank"
+          >
+            Buy
+          </a>
+        )}
+        {fiatOut && (
+          <a
+            className="btn-primary rounded w-full w-[135px] h-[40px]"
+            href={fiat.sellUrl}
+            rel="noreferrer"
+            target="_blank"
+          >
+            Sell
+          </a>
+        )}
+      </div>
+      <hr className="widget-separator" />
+      <div className="flex">
+        <div className="w-[120px]">Deposit Methods</div>
+        <div className="grid grid-cols-3">
+          {fiat.depositMethods.map((method) => (
+            <div
+              key={method}
+              className="flex items-center mx-12 my-5 text-12 weight-medium"
+            >
+              <IconCheck className="w-14 h-14 p-2 bg-blue-0 rounded-full mr-5 dark:bg-blue-1" />
+              {method}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/Fiat.tsx
+++ src/pages/Fiat.tsx
@@ -1,3 +1,33 @@
+import { DepositMethods, FiatBox, Operations } from 'elements/fiat/FiatBox';
+import moonPayLogo from 'assets/logos/moonpay.svg';
+
+const fiats = [
+  {
+    name: 'MoonPay',
+    text: 'MoonPay is a financial technology company that builds payments infrastructure for crypto. Our on-and-off-ramp suite of products provides a seamless experience for converting between fiat currencies and cryptocurrencies using all major payment methods.',
+    logo: moonPayLogo,
+    operations: [Operations.fiatIn, Operations.fiatOut],
+    depositMethods: [
+      DepositMethods.wire,
+      DepositMethods.creditCard,
+      DepositMethods.debitCard,
+      DepositMethods.samsumgPay,
+      DepositMethods.googlePay,
+      DepositMethods.applePay,
+    ],
+    buyUrl:
+      'https://buy.moonpay.com/?apiKey=pk_live_qiJRayRj9iKRUWCfaWX5Vo6Sn0rnNsj&defaultCurrencyCode=ETH',
+    sellUrl:
+      'https://sell.moonpay.com/?apiKey=pk_live_qiJRayRj9iKRUWCfaWX5Vo6Sn0rnNsj&defaultCurrencyCode=ETH',
+  },
+];
+
 export const Fiat = () => {
-  return <h1 className="flex justify-center">Fiat</h1>;
+  return (
+    <div className="grid">
+      {fiats.map((fiat) => (
+        <FiatBox key={fiat.name} fiat={fiat} />
+      ))}
+    </div>
+  );
 };
