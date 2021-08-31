e1fae8ed0eacce3fd8708722caaa02fd84ad2fe5
John Williamson <johnjwilliamson@live.com>
2021-08-31 17:26:09 +1000

    add double modal

---
+++ src/elements/modalDoubleApprove/modalDoubleApprove.tsx
@@ -0,0 +1,74 @@
+import { Token } from 'services/observables/tokens';
+import { ModalApprove } from '../modalApprove/modalApprove';
+import { useState } from 'react';
+
+interface ModalApproveProps {
+  setIsOpen: Function;
+  isOpen: boolean;
+  amounts: string[];
+  fromTokens?: Token[];
+  handleApproved: Function;
+  waitForApproval?: boolean;
+}
+
+export const ModalDoubleApprove = ({
+  setIsOpen,
+  isOpen,
+  amounts,
+  fromTokens,
+  handleApproved,
+  waitForApproval,
+}: ModalApproveProps) => {
+  const [amount1, amount2] = amounts;
+  const [token1, token2] = fromTokens as [Token, Token];
+
+  const [token1IsOpen, setToken1IsOpen] = useState(true);
+  const [token2IsOpen, setToken2IsOpen] = useState(false);
+
+  const token1PromptCompleted = () => {
+    setToken1IsOpen(false);
+    setToken2IsOpen(true);
+  };
+
+  const token2PromptCompleted = () => {
+    setToken2IsOpen(false);
+    setIsOpen(false);
+  };
+
+  const [token1Approved, setToken1IsApproved] = useState(false);
+  const [token2Approved, setToken2IsApproved] = useState(false);
+
+  const token1IsApproved = () => {
+    setToken1IsApproved(true);
+    if (token2Approved) {
+      handleApproved();
+    }
+  };
+  const token2IsApproved = () => {
+    setToken2IsApproved(true);
+    if (token1Approved) {
+      handleApproved();
+    }
+  };
+
+  return (
+    <>
+      <ModalApprove
+        setIsOpen={token1PromptCompleted}
+        isOpen={isOpen && token1IsOpen}
+        waitForApproval={waitForApproval}
+        handleApproved={token1IsApproved}
+        fromToken={token1}
+        amount={amount1}
+      />
+      <ModalApprove
+        setIsOpen={token2PromptCompleted}
+        isOpen={isOpen && token2IsOpen}
+        waitForApproval={waitForApproval}
+        handleApproved={token2IsApproved}
+        fromToken={token2}
+        amount={amount2}
+      />
+    </>
+  );
+};
