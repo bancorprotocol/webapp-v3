f4a4f5c295e37115a948213ff4d83c89e61407ec
Jan Langheimer <langheimer@gmail.com>
2021-09-14 11:46:24 +0200

    move actions cell to function

--- src/elements/earn/pools/PoolsTable.tsx
+++ src/elements/earn/pools/PoolsTable.tsx
@@ -76,6 +76,39 @@ export const PoolsTable = ({ pools, search, setSearch }: Props) => {
     );
   };
 
+  const CellActions = () => {
+    return (
+      <div className="flex">
+        <NavLink
+          to="/"
+          className="btn-primary btn-sm rounded-[12px] !w-[35px] !h-[35px] p-0 shadow-header mr-10"
+        >
+          <Tooltip
+            content="Coming Soon"
+            button={
+              <IconPlus
+                className={`w-20 hover:rotate-180 transition-transform duration-300`}
+              />
+            }
+          />
+        </NavLink>
+        <NavLink
+          to="/"
+          className="btn-outline-primary btn-sm rounded-[12px] !w-[35px] !h-[35px] p-0 border shadow-header"
+        >
+          <Tooltip
+            content="Trade"
+            button={
+              <IconSync
+                className={`w-20 hover:rotate-180 transition-transform duration-300`}
+              />
+            }
+          />
+        </NavLink>
+      </div>
+    );
+  };
+
   const columns = useMemo<TableColumn<Pool>[]>(
     () => [
       {
@@ -147,36 +180,7 @@ export const PoolsTable = ({ pools, search, setSearch }: Props) => {
       {
         id: 'actions',
         Header: '',
-        accessor: () => (
-          <div className="flex">
-            <NavLink
-              to="/"
-              className="btn-primary btn-sm rounded-[12px] !w-[35px] !h-[35px] p-0 shadow-header mr-10"
-            >
-              <Tooltip
-                content="Coming Soon"
-                button={
-                  <IconPlus
-                    className={`w-20 hover:rotate-180 transition-transform duration-300`}
-                  />
-                }
-              />
-            </NavLink>
-            <NavLink
-              to="/"
-              className="btn-outline-primary btn-sm rounded-[12px] !w-[35px] !h-[35px] p-0 border shadow-header"
-            >
-              <Tooltip
-                content="Trade"
-                button={
-                  <IconSync
-                    className={`w-20 hover:rotate-180 transition-transform duration-300`}
-                  />
-                }
-              />
-            </NavLink>
-          </div>
-        ),
+        Cell: () => CellActions(),
         width: 50,
         minWidth: 50,
         disableSortBy: true,
