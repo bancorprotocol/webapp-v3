d1479afed422aaf0f0fe2b17f661e49599084d89
Jan Langheimer <langheimer@gmail.com>
2021-09-30 14:44:45 +0200

    add loading and error screens for add liquidity

--- src/elements/earn/pools/addLiquidity/AddLiquidityError.tsx
+++ src/elements/earn/pools/addLiquidity/AddLiquidityError.tsx
@@ -3,7 +3,12 @@ import { Widget } from 'components/widgets/Widget';
 export const AddLiquidityError = () => {
   return (
     <Widget title="Add Liquidity">
-      <div>ERROR</div>
+      <div className="bg-error rounded p-20 text-white text-center">
+        <div className="font-semibold mb-10">
+          Invalid Pool or Pool not found!
+        </div>
+        <div>Please try again or contact support.</div>
+      </div>
     </Widget>
   );
 };
--- src/elements/earn/pools/addLiquidity/AddLiquidityLoading.tsx
+++ src/elements/earn/pools/addLiquidity/AddLiquidityLoading.tsx
@@ -3,7 +3,11 @@ import { Widget } from 'components/widgets/Widget';
 export const AddLiquidityLoading = () => {
   return (
     <Widget title="Add Liquidity">
-      <div>LOADING</div>
+      <div className="space-y-10">
+        <div className="loading-skeleton !rounded h-[50px]"></div>
+        <div className="loading-skeleton !rounded h-[300px]"></div>
+        <div className="loading-skeleton !rounded h-[50px]"></div>
+      </div>
     </Widget>
   );
 };
