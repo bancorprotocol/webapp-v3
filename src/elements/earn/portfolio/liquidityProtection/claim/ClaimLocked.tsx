62a847d72ee26103c26500a640d727211ab86e94
Ran Cohen <ran@bancor.network>
2021-10-25 13:41:07 +0300

    Fixed locked positions UI when multiple positions are present, added locked icon

--- src/elements/earn/portfolio/liquidityProtection/claim/ClaimLocked.tsx
+++ src/elements/earn/portfolio/liquidityProtection/claim/ClaimLocked.tsx
@@ -1,7 +1,7 @@
 import { Image } from '../../../../../components/image/Image';
 import { Token } from 'services/observables/tokens';
 import { LockedBnt } from 'services/web3/lockedbnt/lockedbnt';
-import { ReactComponent as IconLock } from 'assets/icons/lock.svg';
+import { ReactComponent as IconLock } from 'assets/icons/locked.svg';
 import { prettifyNumber } from 'utils/helperFunctions';
 import { CountdownTimer } from 'components/countdownTimer/CountdownTimer';
 import { UTCTimestamp } from 'lightweight-charts';
@@ -33,33 +33,40 @@ export const ClaimLocked = ({ bnt, locked }: ClaimLockedProps) => {
             </div>
           ) : (
             <>
-              {locked.map((lock) => (
-                <div
-                  key={lock.expiry.toString()}
-                  className="w-full flex justify-between items-center"
-                >
-                  <div className="flex items-center">
-                    <IconLock className="h-20 mr-16" />
-                    <Image
-                      alt="BNT Logo"
-                      className="w-30 mr-10"
-                      src={bnt?.logoURI}
-                    />
-                    <div className="mr-10">{bnt?.symbol}</div>
-                    <div className="mr-10">{prettifyNumber(lock.bnt)}</div>
-                    <div className="text-12 text-primary dark:text-primary-light">{`(~${prettifyNumber(
-                      bnt && bnt.usdPrice ? Number(bnt.usdPrice) * lock.bnt : 0,
-                      true
-                    )})`}</div>
-                  </div>
-                  <div className="flex">
-                    <div className="text-primary dark:text-primary-light font-semibold mr-10">
-                      <CountdownTimer
-                        date={lock.expiry.unix() as UTCTimestamp}
+              {locked.map((lock, index) => (
+                <div>
+                  <div
+                    key={lock.expiry.toString()}
+                    className="w-full flex justify-between items-center"
+                  >
+                    <div className="flex items-center">
+                      <IconLock className="h-20 mr-16" />
+                      <Image
+                        alt="BNT Logo"
+                        className="w-30 mr-10"
+                        src={bnt?.logoURI}
                       />
+                      <div className="mr-10">{bnt?.symbol}</div>
+                      <div className="mr-10">{prettifyNumber(lock.bnt)}</div>
+                      <div className="text-12 text-primary dark:text-primary-light">{`(~${prettifyNumber(
+                        bnt && bnt.usdPrice
+                          ? Number(bnt.usdPrice) * lock.bnt
+                          : 0,
+                        true
+                      )})`}</div>
                     </div>
-                    left to claim
+                    <div className="flex text-12">
+                      <div className="text-primary dark:text-primary-light font-semibold mr-10 text-14">
+                        <CountdownTimer
+                          date={lock.expiry.unix() as UTCTimestamp}
+                        />
+                      </div>
+                      left to claim
+                    </div>
                   </div>
+                  {index !== locked.length - 1 && (
+                    <hr className="content-separator my-16" />
+                  )}
                 </div>
               ))}
             </>
