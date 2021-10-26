1ac1cd0b0d24c800205cea458d48af179bf62494
Ran Cohen <ran@bancor.network>
2021-10-26 16:15:40 +0300

    removed stale imports

--- src/pages/earn/portfolio/rewards/RewardsClaim.tsx
+++ src/pages/earn/portfolio/rewards/RewardsClaim.tsx
@@ -3,14 +3,6 @@ import { RewardsClaimAmount } from 'elements/earn/portfolio/liquidityProtection/
 import { RewardsClaimInfo } from 'elements/earn/portfolio/liquidityProtection/rewards/claim/RewardsClaimInfo';
 import { useRewardsClaim } from 'elements/earn/portfolio/liquidityProtection/rewards/claim/useRewardsClaim';
 import { RewardsClaimCTA } from 'elements/earn/portfolio/liquidityProtection/rewards/claim/RewardsClaimCTA';
-import { useWeb3React } from '@web3-react/core';
-import { useInterval } from 'hooks/useInterval';
-import { useState, useEffect } from 'react';
-import { useAppSelector } from 'redux/index';
-import {
-  fetchPendingRewards,
-  claimRewards,
-} from 'services/web3/protection/rewards';
 
 export const RewardsClaim = () => {
   const [claimableRewards, account] = useRewardsClaim();
