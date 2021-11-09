d7eb67afdc47fd023226aaa9ee22c1ac3bcdca44
Ran Cohen <ran@bancor.network>
2021-11-09 17:31:32 +0200

    simplified auto connect to one use effect

--- src/services/web3/wallet/hooks.ts
+++ src/services/web3/wallet/hooks.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useWeb3React } from '@web3-react/core';
-import { injected, gnosisSafe } from 'services/web3/wallet/connectors';
+import { gnosisSafe, injected } from 'services/web3/wallet/connectors';
 import { getAutoLoginLS, setAutoLoginLS } from 'utils/localStorage';
 import { IS_IN_IFRAME } from 'utils/helperFunctions';
 
@@ -9,28 +9,16 @@ export const useAutoConnect = () => {
 
   const [triedAutoLogin, setTriedAutoLogin] = useState(false);
 
-  // if we're not in an iFrame we start with true and never make a redundant call
-  const [triedSafe, setTriedSafe] = useState(!IS_IN_IFRAME);
-
-  // first, try connecting to a gnosis safe
   useEffect(() => {
-    if (!triedSafe) {
+    if (IS_IN_IFRAME)
       gnosisSafe.isSafeApp().then((loadedInSafe) => {
         if (loadedInSafe) {
           activate(gnosisSafe, undefined, true).catch(() => {
-            setTriedSafe(true);
             setTriedAutoLogin(true);
           });
-        } else {
-          setTriedSafe(true);
         }
       });
-    }
-  }, [activate, setTriedSafe]); // intentionally only running on mount
-
-  // then, if that fails, try connecting to an injected connector
-  useEffect(() => {
-    if (triedSafe && getAutoLoginLS())
+    else if (getAutoLoginLS())
       injected.isAuthorized().then((isAuthorized: boolean) => {
         if (isAuthorized) {
           activate(injected, undefined, true)
