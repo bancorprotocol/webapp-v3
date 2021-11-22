4e4237d37f89aec1bcdb8b6bf84821af0eb3d4af
Ran Cohen <ran@bancor.network>
2021-11-22 17:14:23 +0200

    added not found icon (testing colors)

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,3 +1,9 @@
+import { ReactComponent as Icon404 } from 'assets/logos/404.svg';
+
 export const NotFound = () => {
-  return <h1 className="flex justify-center">Not Found</h1>;
+  return (
+    <div>
+      <Icon404 className="w-[1000px] h-[700px] mx-auto" />
+    </div>
+  );
 };
