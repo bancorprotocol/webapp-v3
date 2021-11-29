3cd40d2dc5ce18b553e38c13e19cce4baf32665f
Jan Langheimer <langheimer@gmail.com>
2021-11-29 14:22:39 +0100

    add usecallback and remove arrow functions

--- src/elements/marketingBanner/MarketingBanner.tsx
+++ src/elements/marketingBanner/MarketingBanner.tsx
@@ -3,13 +3,18 @@ import { ReactComponent as IconBancorv3 } from 'assets/logos/bancorv3.svg';
 import { ReactComponent as IconSlogan } from 'assets/logos/discoverDefiSlogan.svg';
 import { useDispatch } from 'react-redux';
 import { setShowBanner } from 'redux/user/user';
+import { useCallback } from 'react';
 
 export const MarketingBanner = () => {
   const dispatch = useDispatch();
-  const handleCloseBanner = (e: any) => {
-    e.preventDefault();
-    dispatch(setShowBanner(false));
-  };
+  const handleCloseBanner = useCallback(
+    (e: any) => {
+      e.preventDefault();
+      dispatch(setShowBanner(false));
+    },
+    [dispatch]
+  );
+
   return (
     <div className="w-full max-w-[1140px] mx-auto sticky top-[80px] z-30">
       <a
@@ -20,7 +25,7 @@ export const MarketingBanner = () => {
       >
         <button
           className="absolute right-20 p-10 text-white"
-          onClick={(e) => handleCloseBanner(e)}
+          onClick={handleCloseBanner}
         >
           <IconTimes className="w-12" />
         </button>
