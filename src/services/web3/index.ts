2d77a696bf09f4fa224cf4f245ec0d552d0fe914
Ran Cohen <ran@bancor.network>
2021-12-06 17:04:56 +0200

    added keeping ws alive method

--- src/services/web3/index.ts
+++ src/services/web3/index.ts
@@ -10,6 +10,13 @@ export const web3 = {
   ),
 };
 
+export const keepWSOpen = () => {
+  keepAlive(web3.provider, (err) => {
+    keepWSOpen();
+    console.error('Web Socket Closed', JSON.stringify(err, null, 2));
+  });
+};
+
 export const writeWeb3 = {
   signer: window.ethereum
     ? new Web3Provider(window.ethereum).getSigner()
@@ -31,3 +38,33 @@ export const buildContract = (
   contractAddress: string,
   injectedWeb3?: Web3Provider | Signer
 ) => new Contract(contractAddress, abi, injectedWeb3 || web3.provider);
+
+const keepAlive = (
+  provider: providers.WebSocketProvider,
+  onDisconnect: (err: any) => void,
+  expectedPongBack = 15000,
+  checkInterval = 7500
+) => {
+  let pingTimeout: NodeJS.Timeout | null = null;
+  let keepAliveInterval: NodeJS.Timeout | null = null;
+
+  provider.on('open', () => {
+    keepAliveInterval = setInterval(() => {
+      provider._websocket.ping();
+
+      pingTimeout = setTimeout(() => {
+        provider._websocket.terminate();
+      }, expectedPongBack);
+    }, checkInterval);
+  });
+
+  provider.on('close', (err: any) => {
+    if (keepAliveInterval) clearInterval(keepAliveInterval);
+    if (pingTimeout) clearTimeout(pingTimeout);
+    onDisconnect(err);
+  });
+
+  provider.on('pong', () => {
+    if (pingTimeout) clearInterval(pingTimeout);
+  });
+};
