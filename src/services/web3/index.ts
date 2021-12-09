2b86ecf23b112b011d8c8c3633261b493f93d894
Ran Cohen <ran@bancor.network>
2021-12-09 10:49:57 +0200

    fixed ws connection init and removed .ws from alchemy url

--- src/services/web3/index.ts
+++ src/services/web3/index.ts
@@ -3,6 +3,8 @@ import { Contract, ContractInterface } from '@ethersproject/contracts';
 import { EthNetworks } from 'services/web3//types';
 import { providers, Signer } from 'ethers';
 import { buildAlchemyUrl } from 'services/web3/wallet/connectors';
+import { currentNetwork$ } from 'services/observables/network';
+import { take } from 'rxjs/operators';
 
 export const web3 = {
   provider: new providers.WebSocketProvider(
@@ -11,7 +13,7 @@ export const web3 = {
 };
 
 export const keepWSOpen = () => {
-  keepAlive(web3.provider, (err) => {
+  keepAlive((err) => {
     keepWSOpen();
     console.error('Web Socket Closed', JSON.stringify(err, null, 2));
   });
@@ -39,32 +41,34 @@ export const buildContract = (
   injectedWeb3?: Web3Provider | Signer
 ) => new Contract(contractAddress, abi, injectedWeb3 || web3.provider);
 
-const keepAlive = (
-  provider: providers.WebSocketProvider,
+const keepAlive = async (
   onDisconnect: (err: any) => void,
   checkInterval = 7500
 ) => {
-  let pingTimeout: NodeJS.Timeout | null = null;
   let keepAliveInterval: NodeJS.Timeout | null = null;
 
-  provider._websocket.onopen = () => {
+  //Init
+  const network = await currentNetwork$.pipe(take(1)).toPromise();
+  web3.provider = new providers.WebSocketProvider(buildAlchemyUrl(network));
+
+  //Add Events
+  web3.provider._websocket.onopen = () => {
     keepAliveInterval = setInterval(() => {
       if (
-        provider._websocket.readyState === WebSocket.OPEN ||
-        provider._websocket.readyState === WebSocket.CONNECTING
+        web3.provider._websocket.readyState === WebSocket.OPEN ||
+        web3.provider._websocket.readyState === WebSocket.CONNECTING
       )
         return;
 
-      provider._websocket.close();
+      web3.provider._websocket.close();
     }, checkInterval);
   };
 
-  provider._websocket.onclose = (err: any) => onError(err);
-  provider._websocket.onerror = (err: any) => onError(err);
+  web3.provider._websocket.onclose = (err: any) => onError(err);
+  web3.provider._websocket.onerror = (err: any) => onError(err);
 
   const onError = (err: any) => {
     if (keepAliveInterval) clearInterval(keepAliveInterval);
-    if (pingTimeout) clearTimeout(pingTimeout);
     onDisconnect(err);
   };
 };
