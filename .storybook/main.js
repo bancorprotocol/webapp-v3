03f3421348d46dbed2ebaf2450356ef990b5b826
Jan Langheimer <langheimer@gmail.com>
2021-12-14 10:09:06 +0100

    feature/storybook: add tailwind support

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,25 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ],
-  "framework": "@storybook/react"
-}
+  framework: '@storybook/react',
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        {
+          loader: 'postcss-loader',
+          options: {
+            ident: 'postcss',
+            plugins: [require('tailwindcss'), require('autoprefixer')],
+          },
+        },
+      ],
+      include: `${__dirname}../`,
+    });
+    return config;
+  },
+};
