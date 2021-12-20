21b6ee16bd73b484eed1d30bdd4ed30777464f4d
Ran Cohen <ran@bancor.network>
2021-12-20 10:35:10 +0200

    Fixed type issues and changing network without alchemy key

--- src/services/web3/index.ts
+++ src/services/web3/index.ts
@@ -6,14 +6,13 @@ import { buildAlchemyUrl } from 'services/web3/wallet/connectors';
 import { currentNetwork$ } from 'services/observables/network';
 import { take } from 'rxjs/operators';
 
-const getProvider = () => {
-  if (process.env.REACT_APP_ALCHEMY_MAINNET) {
-    return new providers.WebSocketProvider(
-      buildAlchemyUrl(EthNetworks.Mainnet)
-    );
-  } else {
-    return providers.getDefaultProvider(EthNetworks.Mainnet);
-  }
+export const getProvider = (
+  network: EthNetworks = EthNetworks.Mainnet
+): providers.WebSocketProvider | providers.BaseProvider => {
+  if (process.env.REACT_APP_ALCHEMY_MAINNET)
+    return new providers.WebSocketProvider(buildAlchemyUrl(network));
+
+  return providers.getDefaultProvider(network);
 };
 
 export const web3 = {
@@ -21,24 +20,31 @@ export const web3 = {
 };
 
 export const keepWSOpen = () => {
-  setInterval(async () => {
-    if (
-      web3.provider._websocket.readyState === WebSocket.OPEN ||
-      web3.provider._websocket.readyState === WebSocket.CONNECTING
-    )
-      return;
+  if (!(web3.provider instanceof providers.WebSocketProvider)) return;
 
-    try {
-      web3.provider._websocket.close();
-    } catch (error) {
-      console.error('Failed closing websocket', error);
-    }
-    try {
-      console.log('Reconnecting websocket');
-      const network = await currentNetwork$.pipe(take(1)).toPromise();
-      web3.provider = new providers.WebSocketProvider(buildAlchemyUrl(network));
-    } catch (error) {
-      console.error('Failed init web3', error);
+  setInterval(async () => {
+    //Extra check since TS doesnt detect things outside interval
+    if (web3.provider instanceof providers.WebSocketProvider) {
+      if (
+        web3.provider._websocket.readyState === WebSocket.OPEN ||
+        web3.provider._websocket.readyState === WebSocket.CONNECTING
+      )
+        return;
+
+      try {
+        web3.provider._websocket.close();
+      } catch (error) {
+        console.error('Failed closing websocket', error);
+      }
+      try {
+        console.log('Reconnecting websocket');
+        const network = await currentNetwork$.pipe(take(1)).toPromise();
+        web3.provider = new providers.WebSocketProvider(
+          buildAlchemyUrl(network)
+        );
+      } catch (error) {
+        console.error('Failed init web3', error);
+      }
     }
   }, 7500);
 };
@@ -51,7 +57,9 @@ export const writeWeb3 = {
       ).getSigner(),
 };
 
-export const setProvider = (provider: providers.WebSocketProvider) => {
+export const setProvider = (
+  provider: providers.WebSocketProvider | providers.BaseProvider
+) => {
   web3.provider = provider;
 };
 
