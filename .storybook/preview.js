7fe5a3117da869b95a0e379e093b4848dfa7aebb
Jan Langheimer <langheimer@gmail.com>
2022-01-04 08:39:29 +0100

    storybook: cleanup

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -10,7 +10,7 @@ export const parameters = {
   },
   options: {
     storySort: {
-      order: ['Foundation', 'Components', '*'],
+      order: ['DesignSystem', 'Components', '*'],
     },
   },
 };
