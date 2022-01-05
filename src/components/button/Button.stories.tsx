e60ff98eaa7cd15cc1f80804699df52aa2a56d6a
Jan Langheimer <langheimer@gmail.com>
2022-01-05 10:55:37 +0100

    storybook: Button.stories.tsx add variants and playground

--- src/components/button/Button.stories.tsx
+++ src/components/button/Button.stories.tsx
@@ -1,6 +1,6 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { withDesign } from 'storybook-addon-designs';
-import { Button } from './Button';
+import { Button, ButtonSize, ButtonVariant } from './Button';
 
 export default {
   title: 'Components/Buttons',
@@ -8,22 +8,69 @@ export default {
   decorators: [withDesign],
 } as ComponentMeta<typeof Button>;
 
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+export const Variants: ComponentStory<typeof Button> = (args) => {
+  return (
+    <div className="space-y-20">
+      {Object.values(ButtonVariant).map((variant) => {
+        return (
+          <div key={variant} className="space-y-10">
+            <h2 className="uppercase">{variant}</h2>
+            <div className="flex items-center space-x-20">
+              <div className="min-w-[120px]">DEFAULT</div>
+              {Object.values(ButtonSize).map((size) => {
+                return (
+                  <Button key={size} {...args} variant={variant} size={size}>
+                    Text
+                  </Button>
+                );
+              })}
+            </div>
+            <div className="flex items-center space-x-20">
+              <div className="min-w-[120px]">DISABLED</div>
+              {Object.values(ButtonSize).map((size) => {
+                return (
+                  <Button
+                    key={size}
+                    {...args}
+                    variant={variant}
+                    size={size}
+                    disabled
+                  >
+                    Text
+                  </Button>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export const ButtonMain = Template.bind({});
+Variants.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/37rEn9Up8BK377t7KFQZ91/Bancor-Design-System?node-id=103%3A1425',
+  },
+};
 
-ButtonMain.args = {
+export const Playground: ComponentStory<typeof Button> = (args) => (
+  <Button {...args} />
+);
+
+Playground.args = {
   children: 'Primary Button',
-  variant: 'primary',
-  size: 'md',
+  variant: ButtonVariant.PRIMARY,
+  size: ButtonSize.MEDIUM,
   outlined: false,
   disabled: false,
   className: '',
 };
 
-ButtonMain.parameters = {
+Playground.parameters = {
   design: {
     type: 'figma',
-    url: 'https://www.figma.com/file/fwADI9wqDrRAdlMX8EddCw/Bancor-v3?node-id=7879%3A257081',
+    url: 'https://www.figma.com/file/37rEn9Up8BK377t7KFQZ91/Bancor-Design-System?node-id=103%3A1425',
   },
 };
