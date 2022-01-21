6d99cc8483110880f343dd6ebc62fe27c73a3275
Ran Cohen <ran@bancor.network>
2022-01-21 19:58:05 +0100

    Fixed colors file

--- src/styles/plugins/colors.js
+++ src/styles/plugins/colors.js
@@ -5,43 +5,16 @@ module.exports = {
     dark: '#08A2C3',
     hover: '#08A2C3',
   },
-
-  info: {
-    DEFAULT: '#3A86FF',
-    50: '#f5f9ff',
-    100: '#ebf3ff',
-    200: '#cee1ff',
-    300: '#b0cfff',
-    400: '#75aaff',
-    500: '#3A86FF',
-    600: '#3479e6',
-    700: '#2c65bf',
-    800: '#235099',
-    900: '#1c427d',
-  },
-
   success: {
     DEFAULT: '#67D79F',
     hover: '#30b674',
   },
-
   warning: {
     DEFAULT: '#FF8A00',
     hover: '#c97307',
   },
-
   error: {
-    DEFAULT: '#FF3F56',
-    50: '#fff5f7',
-    100: '#ffecee',
-    200: '#ffcfd5',
-    300: '#ffb2bb',
-    400: '#ff7989',
-    500: '#FF3F56',
-    600: '#e6394d',
-    700: '#bf2f41',
-    800: '#992634',
-    900: '#7d1f2a',
+    DEFAULT: '#E27D8A',
     hover: '#D86371',
   },
   fog: { DEFAULT: '#F4F4F4' },
@@ -56,8 +29,6 @@ module.exports = {
     low: '#8018181A', //50% Opacity
     disabled: '#3318181A', //20% Opacity
   },
-
-  secondary: { DEFAULT: '#F2F5F9' },
   white: {
     DEFAULT: '#FFFFFF', //100% Opacity
     medium: '#CCFFFFFF', //80% Opacity
