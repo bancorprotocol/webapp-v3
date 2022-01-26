0a1fbfd8193976eb67db5001cc8f4ed123d48c8c
Jan Langheimer <langheimer@gmail.com>
2022-01-26 10:56:58 +0100

    feat(portfolio): add version tabs

--- src/pages/earn/portfolio/Portfolio.tsx
+++ src/pages/earn/portfolio/Portfolio.tsx
@@ -1,57 +1,72 @@
 import { LiquidityProtection } from 'elements/earn/portfolio/liquidityProtection/LiquidityProtection';
-import { useState } from 'react';
 import { PoolTokens } from 'elements/earn/portfolio/poolTokens/PoolTokens';
-import { classNameGenerator } from 'utils/pureFunctions';
 import { useAppSelector } from 'redux/index';
 import { PoolToken } from 'services/observables/tokens';
 import { V3Portfolio } from 'elements/earn/portfolio/v3/V3Portfolio';
+import { Tab } from '@headlessui/react';
+import { classNameGenerator } from 'utils/pureFunctions';
 
 export const Portfolio = () => {
-  const [selectedTab, setSelectedTab] = useState<'v3' | 'v2' | 'v1'>('v3');
-  const poolTokens = useAppSelector<PoolToken[]>(
-    (state) => state.liquidity.poolTokens
+  const v2 = useAppSelector<PoolToken[]>(
+    (state) => state.liquidity.protectedPositions
   );
+  const v1 = useAppSelector<PoolToken[]>((state) => state.liquidity.poolTokens);
+
+  const getTabBtnClasses = (selected: boolean, hidden?: boolean) =>
+    classNameGenerator({
+      'px-10 py-5 rounded-10': true,
+      'bg-white': selected,
+      hidden: hidden,
+    });
 
   return (
     <div className="max-w-[1140px] mx-auto bg-fog dark:bg-black">
-      <div className="flex items-center my-20">
-        <h1 className="text-[30px] font-semibold pl-10 md:pl-0">Portfolio</h1>
-        <div className="text-16 ml-20 space-x-20">
-          <button
-            onClick={() => setSelectedTab('v3')}
-            className={`px-10 py-5 ${classNameGenerator({
-              'font-semibold rounded-10 bg-white': selectedTab === 'v3',
-              'font-light': selectedTab !== 'v3',
-            })}`}
-          >
-            V3
-          </button>
-          <button
-            onClick={() => setSelectedTab('v2')}
-            className={`pb-4 w-[170px] text-left ${classNameGenerator({
-              'font-semibold rounded-10 bg-white': selectedTab === 'v2',
-              'font-light': selectedTab !== 'v2',
-            })}`}
-          >
-            V2
-          </button>
-          {!!poolTokens.length && (
-            <button
-              onClick={() => setSelectedTab('v1')}
-              className={`pb-4 w-[110px] text-right ${classNameGenerator({
-                'font-semibold rounded-10 bg-white': selectedTab === 'v1',
-                'font-light': selectedTab !== 'v1',
-              })}`}
-            >
-              V1
-            </button>
-          )}
-        </div>
-      </div>
+      <Tab.Group>
+        <div className="flex items-center my-30">
+          <h1 className="text-[30px] font-semibold pl-10 md:pl-0">Portfolio</h1>
+          <Tab.List className="space-x-10 ml-20">
+            <Tab className={({ selected }) => getTabBtnClasses(selected)}>
+              V3
+            </Tab>
 
-      {selectedTab === 'v3' && <V3Portfolio />}
-      {selectedTab === 'v2' && <LiquidityProtection />}
-      {selectedTab === 'v1' && <PoolTokens />}
+            <Tab
+              className={({ selected }) =>
+                getTabBtnClasses(selected, !v2.length)
+              }
+            >
+              <div className="flex space-x-5">
+                <div>V2</div>
+                <div className="bg-primary rounded-full w-6 h-6" />
+              </div>
+            </Tab>
+
+            <Tab
+              className={({ selected }) =>
+                getTabBtnClasses(selected, !v1.length)
+              }
+            >
+              <div className="flex space-x-5">
+                <div>V1</div>
+                <div className="bg-primary rounded-full w-6 h-6" />
+              </div>
+            </Tab>
+          </Tab.List>
+        </div>
+
+        <Tab.Panels>
+          <Tab.Panel>
+            <V3Portfolio />
+          </Tab.Panel>
+
+          <Tab.Panel>
+            <LiquidityProtection />
+          </Tab.Panel>
+
+          <Tab.Panel>
+            <PoolTokens />
+          </Tab.Panel>
+        </Tab.Panels>
+      </Tab.Group>
     </div>
   );
 };
