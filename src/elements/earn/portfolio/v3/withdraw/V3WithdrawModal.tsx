d1e208897ea53e54c0720e7454ffbf8896421a5d
Jan Langheimer <langheimer@gmail.com>
2022-02-09 11:21:58 +0100

    feature(withdraw wiz): add V3WithdrawModal.stories.tsx

--- src/elements/earn/portfolio/v3/withdraw/V3WithdrawModal.tsx
+++ src/elements/earn/portfolio/v3/withdraw/V3WithdrawModal.tsx
@@ -2,10 +2,10 @@ import { memo, useState } from 'react';
 import { wait } from 'utils/pureFunctions';
 import { useAppSelector } from 'redux/index';
 import ModalFullscreenV3 from 'components/modalFullscreen/modalFullscreenV3';
-import V3WithdrawStep1 from 'elements/earn/portfolio/v3/withdraw/V3WithdrawStep1';
-import V3WithdrawStep3 from 'elements/earn/portfolio/v3/withdraw/V3WithdrawStep3';
-import V3WithdrawStep4 from 'elements/earn/portfolio/v3/withdraw/V3WithdrawStep4';
-import V3WithdrawStep2 from 'elements/earn/portfolio/v3/withdraw/V3WithdrawStep2';
+import V3WithdrawStep1 from 'elements/earn/portfolio/v3/withdraw/step1/V3WithdrawStep1';
+import V3WithdrawStep3 from 'elements/earn/portfolio/v3/withdraw/step3/V3WithdrawStep3';
+import V3WithdrawStep4 from 'elements/earn/portfolio/v3/withdraw/step4/V3WithdrawStep4';
+import V3WithdrawStep2 from 'elements/earn/portfolio/v3/withdraw/step2/V3WithdrawStep2';
 
 interface Props {
   isOpen: boolean;
