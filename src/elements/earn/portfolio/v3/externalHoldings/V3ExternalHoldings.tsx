f0eaa4d8df16d46f6dd18d46332dbb7643abb720
Jan Langheimer <langheimer@gmail.com>
2022-02-14 09:38:23 +0100

    feat(external holdings): move logic to custom hook

--- src/elements/earn/portfolio/v3/externalHoldings/V3ExternalHoldings.tsx
+++ src/elements/earn/portfolio/v3/externalHoldings/V3ExternalHoldings.tsx
@@ -1,19 +1,9 @@
-import { useAppSelector } from 'redux/index';
-import { Token } from 'services/observables/tokens';
-import { memo, useMemo, useState } from 'react';
-import {
-  ApyVisionData,
-  ExternalHolding,
-  fetchExternalHoldings,
-  getExternalHoldingsNonUni,
-  getExternalHoldingsUni,
-} from 'elements/earn/portfolio/v3/externalHoldings/externalHoldings';
-import { useAsyncEffect } from 'use-async-effect';
-import { useWeb3React } from '@web3-react/core';
+import { memo } from 'react';
 import V3ExternalHoldingsItem from 'elements/earn/portfolio/v3/externalHoldings/V3ExternalHoldingsItem';
 import { Navigation } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react/swiper-react';
 import { NavigationOptions } from 'swiper/types';
+import { useExternalHoldings } from 'elements/earn/portfolio/v3/externalHoldings/useExternalHoldings';
 
 const navOptions: NavigationOptions = {
   nextEl: '.external-holding-swiper-next-btn',
@@ -23,40 +13,7 @@ const navOptions: NavigationOptions = {
 };
 
 const V3ExternalHoldings = () => {
-  const { account } = useWeb3React();
-  const [apyVisionData, setApyVisionData] = useState<ApyVisionData>({
-    positionsUni: [],
-    positionsNonUni: [],
-  });
-
-  const allTokens: Token[] = useAppSelector((state) => state.bancor.tokens);
-  const tokensMap = useMemo(
-    () => new Map(allTokens.map((token) => [token.symbol, token])),
-    [allTokens]
-  );
-
-  const positionsUni: ExternalHolding[] = useMemo(
-    () => getExternalHoldingsUni(apyVisionData.positionsUni, tokensMap),
-    [apyVisionData.positionsUni, tokensMap]
-  );
-
-  const positionsNonUni: ExternalHolding[] = useMemo(
-    () => getExternalHoldingsNonUni(apyVisionData.positionsNonUni, tokensMap),
-    [apyVisionData.positionsNonUni, tokensMap]
-  );
-
-  const positions: ExternalHolding[] = useMemo(
-    () => [...positionsUni, ...positionsNonUni],
-    [positionsUni, positionsNonUni]
-  );
-
-  useAsyncEffect(async () => {
-    if (!account) {
-      return;
-    }
-    const apyVisionData = await fetchExternalHoldings(account);
-    setApyVisionData(apyVisionData);
-  }, [account]);
+  const { positions } = useExternalHoldings();
 
   return (
     <section className="content-block p-20">
