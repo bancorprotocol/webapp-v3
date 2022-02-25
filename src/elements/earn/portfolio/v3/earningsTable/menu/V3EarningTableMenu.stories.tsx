9bc7bba45c03fbf9698584a75558e1eb4cfc54f8
Jan Langheimer <langheimer@gmail.com>
2022-02-25 14:05:24 +0100

    feature(earning table menu): add  table menu menu story

---
+++ src/elements/earn/portfolio/v3/earningsTable/menu/V3EarningTableMenu.stories.tsx
@@ -0,0 +1,32 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { withDesign } from 'storybook-addon-designs';
+import { V3EarningsTableMenuContent } from 'elements/earn/portfolio/v3/earningsTable/menu/V3EarningTableMenuContent';
+
+export default {
+  title: 'Elements/V3/Portfolio/EarningsTable/Menu',
+  component: V3EarningsTableMenuContent,
+  decorators: [withDesign],
+} as ComponentMeta<typeof V3EarningsTableMenuContent>;
+
+export const Menu: ComponentStory<typeof V3EarningsTableMenuContent> = (
+  args
+) => {
+  return (
+    <div className="w-screen max-w-[300px]">
+      <div className="overflow-hidden rounded bg-white p-20 border border-silver h-[280px]">
+        <V3EarningsTableMenuContent {...args} />
+      </div>
+    </div>
+  );
+};
+
+Menu.args = {
+  setIsWithdrawModalOpen: () => {},
+};
+
+Menu.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/37rEn9Up8BK377t7KFQZ91/Bancor-Design-System?node-id=338%3A32981',
+  },
+};
