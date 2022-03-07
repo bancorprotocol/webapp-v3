3aecedeced830e34430447575af6a5ce66b78574
Jan Langheimer <langheimer@gmail.com>
2022-03-07 10:05:07 +0100

    fix(fallbackLogo): remove imageOnError handler

--- src/components/image/useFallbackImage.tsx
+++ src/components/image/useFallbackImage.tsx
@@ -5,17 +5,13 @@ export function useFallbackImage(src?: string) {
   const [loaded, setLoaded] = useState(false);
   const [failed, setFailed] = useState(false);
 
-  const imageOnErrorHandler = () => {
-    setFailed(true);
-  };
-
   const source = useMemo(
     () => (src && !failed ? src : ropstenImage),
     [failed, src]
   );
 
   const onError = useMemo(
-    () => (failed ? undefined : imageOnErrorHandler),
+    () => (failed ? undefined : () => setFailed(true)),
     [failed]
   );
 
