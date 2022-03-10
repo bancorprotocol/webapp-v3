1ccd526434481a689e9b9775b33b283244dfa50a
Ran Cohen <ran@bancor.network>
2022-03-10 16:47:54 +0200

    options setter and check icon

--- src/elements/earn/pools/poolsTable/PoolsTableSort.tsx
+++ src/elements/earn/pools/poolsTable/PoolsTableSort.tsx
@@ -1,9 +1,13 @@
 import { Popover, Switch } from '@headlessui/react';
 import { DropdownTransition } from 'components/transitions/DropdownTransition';
+import { ReactComponent as IconCheck } from 'assets/icons/check.svg';
 import { useState } from 'react';
 
+const options = ['Relavence', 'Highest earning', 'Most popular'];
 export const PoolsTableSort = () => {
   const [rewards, setRewards] = useState(false);
+  const [option, setOption] = useState(0);
+
   return (
     <Popover className="hidden md:block relative">
       <Popover.Button className="flex items-center">
@@ -15,22 +19,26 @@ export const PoolsTableSort = () => {
             <div className="text-12 text-black-low dark:text-white-low">
               Sort By
             </div>
-            <div>Relavence</div>
-            <div>Highest earning</div>
-            <div>Most popular</div>
+            {options.map((x, index) => {
+              return (
+                <button
+                  key={x}
+                  className="w-full flex justify-between items-center"
+                  onClick={() => setOption(index)}
+                >
+                  {x}
+                  {index === option && <IconCheck className="h-16 w-16" />}
+                </button>
+              );
+            })}
             <hr className="border-fog" />
             <div className="text-12 text-black-low dark:text-white-low">
               Visibility
             </div>
             <div className="flex items-center justify-between">
               Rewards only
-              <Switch
-                checked={rewards}
-                onChange={() => setRewards(!rewards)}
-                className={'swap-switch'}
-              >
-                <span className="sr-only">Toggle USD Switch</span>
-                <span aria-hidden="true" className={'swap-switch-toggle'} />
+              <Switch checked={rewards} onChange={() => setRewards(!rewards)}>
+                <span>Toggle USD Switch</span>
               </Switch>
             </div>
           </div>
