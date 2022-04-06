528585c90a8632ee39a842bb7997935411f3252a
Ran Cohen <ran@bancor.network>
2022-04-06 14:54:38 +0300

    fixed migrate and protect layout, added selected holding layout

--- src/pages/earn/portfolio/MigrateProtect.tsx
+++ src/pages/earn/portfolio/MigrateProtect.tsx
@@ -1,6 +1,7 @@
 import { ReactComponent as LogoPWelcome } from 'assets/portfolio/portfolioWelcome.svg';
 import { ReactComponent as IconProtected } from 'assets/icons/protected.svg';
 import { ReactComponent as IconProtectedHeart } from 'assets/icons/protectedHeart.svg';
+import { ReactComponent as IconInfo } from 'assets/icons/info.svg';
 import { Rating } from 'components/rating/Rating';
 import { Button, ButtonVariant } from 'components/button/Button';
 import { ExternalHolding } from 'elements/earn/portfolio/v3/externalHoldings/externalHoldings.types';
@@ -12,9 +13,11 @@ export const MigrateProtect = () => {
   const migrate = true;
 
   return (
-    <div className="grid grid-cols-4 items-center h-[700px]">
-      {migrate ? <Migrate /> : <Protect />}
-      <div>
+    <div className="grid grid-cols-4 h-[700px]">
+      <div className="col-span-3 mr-[170px]">
+        {migrate ? <Migrate /> : <Protect />}
+      </div>
+      <div className="my-auto">
         <LogoPWelcome className="w-[240px] h-[300px]" />
         <div>
           <div className="flex items-center gap-4 mb-20">
@@ -37,6 +40,7 @@ export const MigrateProtect = () => {
 
 const Migrate = () => {
   const [seeAllHoldings, setSeeAllHoldings] = useState(false);
+  const [selectedHolding, setSelectedHolding] = useState(0);
 
   const holdings = [
     {
@@ -53,28 +57,48 @@ const Migrate = () => {
     },
   ];
   return (
-    <div className="col-span-3">
-      <div className="text-4xl w-[550px] mb-20">
-        Migrate your holdings that are at risk of Impermanet loss?
-      </div>
-      {!seeAllHoldings && (
-        <button
-          className="text-black-low"
-          onClick={() => setSeeAllHoldings(true)}
-        >
-          See my other holdings at risk {'->'}
-        </button>
+    <>
+      {selectedHolding === -1 ? (
+        <>
+          <div className="text-4xl w-[550px] mb-20">
+            Migrate your holdings that are at risk of Impermanet loss?
+          </div>
+          {!seeAllHoldings && (
+            <button
+              className="text-black-low"
+              onClick={() => setSeeAllHoldings(true)}
+            >
+              See my other holdings at risk {'->'}
+            </button>
+          )}
+          <MigrateHoldingAtRisk
+            className="mt-60"
+            holdings={seeAllHoldings ? holdings : [holdings[0]]}
+          />
+          <div className="flex items-center mt-[70px]">
+            <Button className="w-[170px]">Yes</Button>
+            <Button variant={ButtonVariant.SECONDARY}>No Thanks</Button>
+          </div>
+        </>
+      ) : (
+        <>
+          <button className="mt-40" onClick={() => setSelectedHolding(-1)}>
+            {'<-'} Back
+          </button>
+          <div className="text-4xl mt-30">
+            Secure this balancer holding from impermanent loss
+          </div>
+          <div className="text-16 text-black-low mt-10">
+            You've lost $15,000 in impermanent loss so far, get 100% protected
+            on bancor.
+          </div>
+          <div className="flex items-center text-black-low mt-40">
+            Moving to protected earnings
+            <IconInfo className="w-[15px] h-[15px] ml-5" />
+          </div>
+        </>
       )}
-      <MigrateHoldingAtRisk
-        className="mt-60"
-        holdings={seeAllHoldings ? holdings : [holdings[0]]}
-      />
-
-      <div className="flex items-center mt-[70px]">
-        <Button className="w-[170px]">Yes</Button>
-        <Button variant={ButtonVariant.SECONDARY}>No Thanks</Button>
-      </div>
-    </div>
+    </>
   );
 };
 
@@ -115,7 +139,7 @@ const MigrateHoldingAtRisk = ({
 
 const Protect = () => {
   return (
-    <div className="col-span-3">
+    <>
       <div className="text-4xl w-[550px] mb-20">
         Would you like to earn 85% on your 25.45 ETH?
       </div>
@@ -124,6 +148,6 @@ const Protect = () => {
         <Button className="w-[170px]">Protect and earn 85%</Button>
         <Button variant={ButtonVariant.SECONDARY}>No Thanks</Button>
       </div>
-    </div>
+    </>
   );
 };
