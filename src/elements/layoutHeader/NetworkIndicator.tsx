9e69f2778ce770fb9d0bfa530f0877161f5b216f
Jan Langheimer <langheimer@gmail.com>
2022-04-12 11:18:09 +0200

    hotfix: add link to admin page

--- src/elements/layoutHeader/NetworkIndicator.tsx
+++ src/elements/layoutHeader/NetworkIndicator.tsx
@@ -1,8 +1,15 @@
+import { useNavigation } from 'services/router';
+
 export const NetworkIndicator = () => {
+  const { pushAdmin } = useNavigation();
+
   return (
-    <div className="flex items-center h-[35px] px-20 text-12 rounded-full bg-fog border border-graphite text-charcoal dark:text-white text-opacity-50 dark:text-opacity-50 dark:border-grey dark:bg-black">
+    <button
+      onClick={pushAdmin}
+      className="flex items-center h-[35px] px-20 text-12 rounded-full bg-fog border border-graphite text-charcoal dark:text-white text-opacity-50 dark:text-opacity-50 dark:border-grey dark:bg-black"
+    >
       <div className={`bg-error w-6 h-6 rounded-full mr-10`} />
       Tenderly Fork
-    </div>
+    </button>
   );
 };
