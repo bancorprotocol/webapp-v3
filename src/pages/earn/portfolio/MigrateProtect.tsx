7130505e91220510706060e6a74af9f8f023c51e
Ran Cohen <ran@bancor.network>
2022-04-14 16:56:34 +0300

    added selected position in protect

--- src/pages/earn/portfolio/MigrateProtect.tsx
+++ src/pages/earn/portfolio/MigrateProtect.tsx
@@ -10,13 +10,14 @@ import { TokensOverlap } from 'components/tokensOverlap/TokensOverlap';
 import { prettifyNumber } from 'utils/helperFunctions';
 import { Switch } from 'components/switch/Switch';
 import { TokenBalance } from 'components/tokenBalance/TokenBalance';
+import { ReactComponent as IconLink } from 'assets/icons/link.svg';
 
 export const MigrateProtect = () => {
-  const migrate = true;
+  const migrate = false;
 
   return (
     <div className="grid grid-cols-4 h-[700px]">
-      <div className="col-span-3 mr-[170px]">
+      <div className="col-span-3 mr-[170px] w-[550px] my-auto">
         {migrate ? <Migrate /> : <Protect />}
       </div>
       <div className="my-auto">
@@ -42,7 +43,7 @@ export const MigrateProtect = () => {
 
 const Migrate = () => {
   const [seeAllHoldings, setSeeAllHoldings] = useState(false);
-  const [selectedHolding, setSelectedHolding] = useState(0);
+  const [selectedHolding, setSelectedHolding] = useState(-1);
 
   const holdings = [
     {
@@ -62,7 +63,7 @@ const Migrate = () => {
     <>
       {selectedHolding === -1 ? (
         <>
-          <div className="text-4xl w-[550px] mb-20">
+          <div className="text-4xl mb-20">
             Migrate your holdings that are at risk of Impermanet loss?
           </div>
           {!seeAllHoldings && (
@@ -160,7 +161,7 @@ const MigrateHoldingAtRisk = ({
           <button
             key={holding.ammName}
             onClick={() => onSelect(index)}
-            className="flex items-center justify-between rounded-20 border border-silver p-20 h-[70px] w-[550px] mb-10"
+            className="flex items-center justify-between rounded-20 border border-silver p-20 h-[70px] w-full mb-10"
           >
             <div>
               <div>{holding.ammName}</div>
@@ -183,16 +184,89 @@ const MigrateHoldingAtRisk = ({
 };
 
 const Protect = () => {
+  const [seeAll, setSeeAll] = useState(false);
+  const [selectedPosition, setSelectedPosition] = useState(0);
+
   return (
     <>
-      <div className="text-4xl w-[550px] mb-20">
-        Would you like to earn 85% on your 25.45 ETH?
-      </div>
-      <div>See my other tokens {'->'}</div>
-      <div className="flex items-center mt-[70px]">
-        <Button className="w-[170px]">Protect and earn 85%</Button>
-        <Button variant={ButtonVariant.SECONDARY}>No Thanks</Button>
-      </div>
+      {selectedPosition === -1 ? (
+        <>
+          {seeAll ? (
+            <>
+              <button className="mt-40" onClick={() => setSeeAll(false)}>
+                {'<-'} Back
+              </button>
+              <div className="text-4xl mt-30">Earn interest on your tokens</div>
+              <div className="text-16 text-black-low mt-10">
+                $10,020 balance
+              </div>
+
+              <button className="flex items-center justify-between rounded-20 border border-silver p-20 h-[70px] mt-10 w-full">
+                <TokenBalance
+                  symbol={'Symbol'}
+                  amount={'100'}
+                  usdPrice={'1000000'}
+                  imgUrl={'token.logoURI'}
+                />
+                <div className="flex items-center gap-10 text-primary">
+                  Earn 86%
+                </div>
+              </button>
+            </>
+          ) : (
+            <>
+              <div className="text-4xl mb-20">
+                Would you like to earn 85% on your 25.45 ETH?
+              </div>
+              <button onClick={() => setSeeAll(true)}>
+                See my other tokens {'->'}
+              </button>
+              <div className="flex items-center mt-[70px]">
+                <Button className="w-[170px]">Protect and earn 85%</Button>
+                <Button variant={ButtonVariant.SECONDARY}>No Thanks</Button>
+              </div>
+            </>
+          )}
+        </>
+      ) : (
+        <>
+          <button className="mt-40" onClick={() => setSelectedPosition(-1)}>
+            {'<-'} Back
+          </button>
+          <div className="text-4xl mt-30">
+            How much of your 25.455 ETH do you want to desposit?
+          </div>
+          <div className="text-16 text-black-low mt-10">$10,020 balance</div>
+          {/* <TokenInputV3
+            token={undefined}
+            inputTkn={undefined}
+            setInputTkn={() => {}}
+          /> */}
+          <div className="rounded-20 bg-silver p-20 h-[80px] mt-20">
+            <div className="flex items-center justify-between text-black-medium">
+              <div>
+                Access full earnings
+                <div className="text-black-low">additional gas ~$1000.00</div>
+              </div>
+              <div className="flex items-center gap-10">
+                40%
+                <Switch selected={true} onChange={() => {}} />
+              </div>
+            </div>
+          </div>
+          <Button className="mt-30 w-[160px]">Deposit</Button>
+
+          <a
+            href={''}
+            target="_blank"
+            className="flex items-center text-black-low font-semibold mt-20"
+            rel="noreferrer"
+          >
+            100% Protected • 7 day cooldown • 0.25% withdrawal fee{' '}
+            <IconLink className="w-14 ml-6" />
+          </a>
+        </>
+      )}
     </>
   );
 };
