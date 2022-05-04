33ebbf2d893daa756f4391ceca3b8fb3f024b18c
Jan Langheimer <langheimer@gmail.com>
2022-05-04 12:35:24 +0200

    add statsV3

--- src/services/observables/triggers.ts
+++ src/services/observables/triggers.ts
@@ -1,15 +1,20 @@
-import { keeperDaoTokens$ } from 'services/observables/tokens';
+import {
+  allTokensNew$,
+  keeperDaoTokens$,
+  tokensNew$,
+} from 'services/observables/tokens';
 import {
   setAllStandardRewardPrograms,
   setAllTokenListTokens,
   setAllTokens,
   setKeeperDaoTokens,
+  setStatisticsV3,
   setTokens,
 } from 'store/bancor/bancor';
 import { getTokenListLS, setTokenListLS } from 'utils/localStorage';
 import { loadingLockedBnt$, loadingPositions$, loadingRewards$ } from './user';
-import { statistics$ } from 'services/observables/statistics';
-import { setv2Pools, setStats, setv3Pools } from 'store/bancor/pool';
+import { statistics$, statisticsV3$ } from 'services/observables/statistics';
+import { setStats, setv2Pools, setv3Pools } from 'store/bancor/pool';
 import {
   setLoadingLockedBnt,
   setLoadingPositions,
@@ -37,7 +42,6 @@ import {
   portfolioWithdrawals$,
   portfolioWithdrawalSettings$,
 } from 'services/observables/portfolio';
-import { allTokensNew$, tokensNew$ } from 'services/observables/tokens';
 import {
   listOfLists,
   tokenListTokens$,
@@ -80,6 +84,10 @@ export const subscribeToObservables = (dispatch: any) => {
 
   statistics$.subscribe((stats) => {
     dispatch(setStats(stats));
+  });
+
+  statisticsV3$.subscribe((stats) => {
+    dispatch(setStatisticsV3(stats));
   });
 
   protectedPositions$.subscribe((protectedPositions) => {
