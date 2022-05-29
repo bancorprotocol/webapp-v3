4aa09838755b04eb8c11390ae85e73ad38a55a78
ashachaf <40430285+ashachaf@users.noreply.github.com>
2022-05-29 17:54:44 +0300

    Update V3HoldingsItemStaked.tsx
    updating the way we display rewards information

--- src/elements/earn/portfolio/v3/holdings/V3HoldingsItemStaked.tsx
+++ src/elements/earn/portfolio/v3/holdings/V3HoldingsItemStaked.tsx
@@ -10,11 +10,12 @@ export const V3HoldingsItemStaked = ({ holding }: { holding: Holding }) => {
 
   return (
     <div>
-      <IconGift
+      /* <IconGift
         className={`w-16 ${
           !isDisabled ? 'text-primary' : 'text-secondary'
         } mx-auto mb-5`}
       />
+      */
 
       <div className="text-secondary">Staked</div>
       <div className={`mt-6 mb-10 ${isDisabled ? 'text-secondary' : ''}`}>
