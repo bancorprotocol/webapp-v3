529d988808821a8335f2cd31e66aeb6ab73a29fa
Jan Langheimer <langheimer@gmail.com>
2022-06-09 15:17:55 +0100

    add init withdraw reminder links

--- src/elements/earn/portfolio/v3/initWithdraw/step4/V3WithdrawStep4.tsx
+++ src/elements/earn/portfolio/v3/initWithdraw/step4/V3WithdrawStep4.tsx
@@ -1,13 +1,29 @@
 import { Button, ButtonVariant } from 'components/button/Button';
 import { ReactComponent as IconCheck } from 'assets/icons/circlecheck.svg';
+import { ReactComponent as IconBell } from 'assets/icons/bell.svg';
+import { ReactComponent as IconCalendar } from 'assets/icons/calendar.svg';
 import { memo } from 'react';
+import { CalendarOptions } from 'datebook';
+import { PopoverV3 } from 'components/popover/PopoverV3';
+
+const getCalendarOptions = (): CalendarOptions => {
+  return {
+    title: 'Bancor Withdrawal',
+    description: 'Bancor Withdrawal cooldown ended.',
+    start: new Date('2020-07-04T19:00:00'),
+    end: new Date('2020-07-04T23:30:00'),
+  };
+};
+
+const generateICalendarEvent = () => {};
 
 interface Props {
   onClose: (state: boolean) => void;
   lockDurationInDays: number;
+  requestId: string;
 }
 
-const V3WithdrawStep4 = ({ onClose, lockDurationInDays }: Props) => {
+const V3WithdrawStep4 = ({ onClose, lockDurationInDays, requestId }: Props) => {
   return (
     <div className="text-center">
       <span className="flex justify-center text-primary items-center text-20">
@@ -16,7 +32,8 @@ const V3WithdrawStep4 = ({ onClose, lockDurationInDays }: Props) => {
       <h1 className="text-[36px] font-normal my-50 leading-10">
         Remember to come back after {lockDurationInDays} days
       </h1>
-      <div className="flex justify-center">
+
+      <div className="flex flex-col items-center space-y-40">
         <Button
           variant={ButtonVariant.Secondary}
           className="px-50"
@@ -24,6 +41,33 @@ const V3WithdrawStep4 = ({ onClose, lockDurationInDays }: Props) => {
         >
           Return to portfolio
         </Button>
+
+        <div className="space-y-20">
+          <PopoverV3
+            buttonElement={() => (
+              <button className="flex items-center">
+                <IconCalendar className="w-20 mr-20" />
+                Add to calendar
+              </button>
+            )}
+          >
+            <div className="flex flex-col text-left items-start space-y-10">
+              <button>Google Web</button>
+              <button>Outlook Web</button>
+              <button>iCalendar / ICS</button>
+            </div>
+          </PopoverV3>
+
+          <a
+            className="flex items-center"
+            href={`https://app.hal.xyz/recipes/bancor-v3-track-ready-withdrawals?withdrawalId=${requestId}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <IconBell className="w-20 mr-20" />
+            Set a hal.xyz reminder
+          </a>
+        </div>
       </div>
     </div>
   );
