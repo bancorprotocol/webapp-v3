237aee9ac532d909e88a6b6aaff2a328a98afd1e
Ran Cohen <ran@bancor.network>
2022-06-14 12:18:56 +0300

    changed v2 apr to be 24h

--- src/elements/earn/pools/poolsTable/PoolsTableCellApr.tsx
+++ src/elements/earn/pools/poolsTable/PoolsTableCellApr.tsx
@@ -5,7 +5,8 @@ export const PoolsTableCellApr = (pool: Pool) => {
   const aprTwo = pool.reserves[1].rewardApr || 0;
   const symbolOne = pool.reserves[0].symbol;
   const symbolTwo = pool.reserves[1].symbol;
-  const formatApr = (rewardApr: number) => (rewardApr + pool.apr_7d).toFixed(2);
+  const formatApr = (rewardApr: number) =>
+    (rewardApr + pool.apr_24h).toFixed(2);
   return (
     <div className="flex items-center">
       <span>{`${symbolOne} ${formatApr(aprOne)}%`}</span>
