4909eb9f59aa108cd7bbbc6c157b2ee15344b971
Doron Zavelevsky <zavelevsky@gmail.com>
2022-06-14 10:46:05 +0300

    moved to use https in alchemy instead of WS

--- src/services/web3/index.ts
+++ src/services/web3/index.ts
@@ -2,7 +2,7 @@ import { Web3Provider } from '@ethersproject/providers';
 import { EthNetworks } from 'services/web3//types';
 import { providers } from 'ethers';
 import { buildAlchemyUrl } from 'services/web3/wallet/connectors';
-import { isForkAvailable } from './config';
+import { isForkAvailable } from 'services/web3/config';
 import { getTenderlyRpcLS } from 'utils/localStorage';
 
 export const getProvider = (
@@ -13,7 +13,7 @@ export const getProvider = (
     return new providers.StaticJsonRpcProvider(getTenderlyRpcLS());
   }
   if (process.env.REACT_APP_ALCHEMY_MAINNET) {
-    return new providers.WebSocketProvider(buildAlchemyUrl(network));
+    return new providers.JsonRpcProvider(buildAlchemyUrl(network, false));
   }
 
   return providers.getDefaultProvider(network);
@@ -23,40 +23,40 @@ export const web3 = {
   provider: getProvider(),
 };
 
-export const keepWSOpen = () => {
-  if (!(web3.provider instanceof providers.WebSocketProvider)) return;
+// export const keepWSOpen = () => {
+//   if (!(web3.provider instanceof providers.WebSocketProvider)) return;
 
-  setInterval(async () => {
-    //Extra check since TS doesnt detect things outside interval
-    if (web3.provider instanceof providers.WebSocketProvider) {
-      if (
-        web3.provider._websocket.readyState === WebSocket.OPEN ||
-        web3.provider._websocket.readyState === WebSocket.CONNECTING
-      )
-        return;
+//   setInterval(async () => {
+//     //Extra check since TS doesnt detect things outside interval
+//     if (web3.provider instanceof providers.WebSocketProvider) {
+//       if (
+//         web3.provider._websocket.readyState === WebSocket.OPEN ||
+//         web3.provider._websocket.readyState === WebSocket.CONNECTING
+//       )
+//         return;
 
-      try {
-        web3.provider._websocket.close();
-      } catch (error) {
-        console.error('Failed closing websocket', error);
-      }
-      try {
-        console.debug('Reconnecting websocket');
-        web3.provider = new providers.WebSocketProvider(
-          buildAlchemyUrl(EthNetworks.Mainnet)
-        );
-      } catch (error) {
-        console.error('Failed init web3', error);
-      }
-    }
-  }, 7500);
-};
+//       try {
+//         web3.provider._websocket.close();
+//       } catch (error) {
+//         console.error('Failed closing websocket', error);
+//       }
+//       try {
+//         console.debug('Reconnecting websocket');
+//         web3.provider = new providers.WebSocketProvider(
+//           buildAlchemyUrl(EthNetworks.Mainnet)
+//         );
+//       } catch (error) {
+//         console.error('Failed init web3', error);
+//       }
+//     }
+//   }, 7500);
+// };
 
 export const writeWeb3 = {
   signer: window.ethereum
     ? new Web3Provider(window.ethereum).getSigner()
-    : new providers.WebSocketProvider(
-        buildAlchemyUrl(EthNetworks.Mainnet)
+    : new providers.JsonRpcProvider(
+        buildAlchemyUrl(EthNetworks.Mainnet, false)
       ).getSigner(),
 };
 
