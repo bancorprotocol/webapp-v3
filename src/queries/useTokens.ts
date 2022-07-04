5620a4d7141355ef2b5a8f723faa204e12e8ff0a
Jan Langheimer <langheimer@gmail.com>
2022-07-04 11:28:20 +0200

    react-query tmp commit

--- src/queries/useTokens.ts
+++ src/queries/useTokens.ts
@@ -1,14 +1,16 @@
 import { useQuery } from 'react-query';
 import { fetchTokenBalanceMulticall } from 'services/web3/token/token';
 import { useApiPoolsV3 } from 'queries/useApiPoolsV3';
+import { useWeb3React } from '@web3-react/core';
 
-export const useTokenBalances = (user?: string) => {
+export const useTokens = () => {
+  const { account } = useWeb3React();
   const { data: pools } = useApiPoolsV3();
   const ids = pools ? pools.map((p) => p.poolDltId) : [];
 
   return useQuery<Map<string, string>>(
-    ['v3', 'token', 'balances', user],
-    () => fetchTokenBalanceMulticall(ids, user!),
-    { enabled: !!user && !!pools }
+    ['v3', 'tokens'],
+    () => fetchTokenBalanceMulticall(ids, account!),
+    { enabled: !!account && !!pools }
   );
 };
