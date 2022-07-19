01ff24da830f28be0cb7b18e63ca49712be36061
Jan Langheimer <langheimer@gmail.com>
2022-07-19 11:54:24 +0200

    remove unneeded debug / test code

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -2,14 +2,11 @@ import { AdminTknData } from 'elements/admin/AdminTknData';
 import { AdminUseMainnet } from 'elements/admin/AdminUseMainnet';
 import { AdminUseFork } from 'elements/admin/AdminUseFork';
 import { AdminControls } from 'elements/admin/AdminControls';
-import { AdminQueryTest } from 'elements/admin/AdminQueryTest';
 
 export const Admin = () => {
   return (
     <div className="space-y-20 text-center">
       <h1 className="pt-80">Bancor Network Configurator</h1>
-
-      <AdminQueryTest />
 
       <hr />
 
