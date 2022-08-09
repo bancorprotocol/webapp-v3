9ce9a109aa8615bdbd8fef3ff3a7147094ea3a0e
Doron Zavelevsky <zavelevsky@gmail.com>
2022-08-09 09:29:16 +0300

    fixed errors and styled

--- src/elements/earn/pools/poolsTable/v3/DepositDisabledModal.tsx
+++ src/elements/earn/pools/poolsTable/v3/DepositDisabledModal.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { ModalV3 } from 'components/modal/ModalV3';
+import { Navigate } from 'components/navigate/Navigate';
 
 interface Props {
   renderButton: (onClick: () => void) => React.ReactNode;
@@ -17,7 +18,17 @@ export const DepositDisabledModal = ({ renderButton }: Props) => {
       {renderButton(() => setIsOpen(true))}
       <ModalV3 setIsOpen={onClose} isOpen={isOpen} large>
         <div className="flex flex-col items-center gap-20 p-20 pb-40 text-center">
-          <div className="text-3xl mb-30">Deposits are currently disabled in the bancor.network UI. To deposit via Etherscan at your own risk, <a href="https://docs.bancor.network/guides/bancor-etherscan-guide/deposit" target="_blank">follow this guide</a>.</div>
+          <div className="leading-7 text-20 mb-30">
+            Deposits are currently disabled in the bancor.network UI. To deposit
+            via Etherscan at your own risk,{' '}
+            <Navigate
+              to="https://docs.bancor.network/guides/bancor-etherscan-guide/deposit"
+              className="text-primary dark:text-primary"
+            >
+              follow this guide
+            </Navigate>
+            .
+          </div>
         </div>
       </ModalV3>
     </>
