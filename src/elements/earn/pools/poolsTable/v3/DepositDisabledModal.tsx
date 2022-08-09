424f1ef1cf9f66da89dc662bc029444a0742f224
ashachaf <40430285+ashachaf@users.noreply.github.com>
2022-08-09 08:54:00 +0300

    Update DepositDisabledModal.tsx

--- src/elements/earn/pools/poolsTable/v3/DepositDisabledModal.tsx
+++ src/elements/earn/pools/poolsTable/v3/DepositDisabledModal.tsx
@@ -17,7 +17,7 @@ export const DepositDisabledModal = ({ renderButton }: Props) => {
       {renderButton(() => setIsOpen(true))}
       <ModalV3 setIsOpen={onClose} isOpen={isOpen} large>
         <div className="flex flex-col items-center gap-20 p-20 pb-40 text-center">
-          <div className="text-3xl mb-30">Deposits are temporarily paused.</div>
+          <div className="text-3xl mb-30">Deposits are currently disabled in the bancor.network UI. To deposit via Etherscan at your own risk, <a href="https://docs.bancor.network/guides/bancor-etherscan-guide/deposit" target="_blank">follow this guide</a>.</div>
         </div>
       </ModalV3>
     </>
