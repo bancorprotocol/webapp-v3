406a6e4f8978b7689186bdd4162ccb29d3d72793
Ran Cohen <ran@bancor.network>
2022-08-25 10:57:12 +0300

    added withdraw and deposit to the page

--- src/elements/earn/portfolio/v3/holdings/V3HoldingPage.tsx
+++ src/elements/earn/portfolio/v3/holdings/V3HoldingPage.tsx
@@ -9,21 +9,65 @@ import {
   getIsLoadingHoldings,
 } from 'store/portfolio/v3Portfolio';
 import { PopoverV3 } from 'components/popover/PopoverV3';
-import { prettifyNumber } from 'utils/helperFunctions';
+import { prettifyNumber, toBigNumber } from 'utils/helperFunctions';
 import { Button, ButtonSize, ButtonVariant } from 'components/button/Button';
+import { useState } from 'react';
+import { bntToken } from 'services/web3/config';
+import { ContractsApi } from 'services/web3/v3/contractsApi';
+import { fetchWithdrawalRequestOutputBreakdown } from 'services/web3/v3/portfolio/withdraw';
+import useAsyncEffect from 'use-async-effect';
+import { expandToken, shrinkToken } from 'utils/formulas';
+import { DepositV3Modal } from 'elements/earn/pools/poolsTable/v3/DepositV3Modal';
+import V3WithdrawModal from '../initWithdraw/V3WithdrawModal';
 
 export const V3HoldingPage = () => {
   const { id } = useParams();
+  const { goToPage } = useNavigation();
+  const [isOpen, setIsOpen] = useState(false);
   const holdings = useAppSelector(getPortfolioHoldings);
   const isLoadingHoldings = useAppSelector(getIsLoadingHoldings);
   const holding = holdings.find((x) => x.pool.poolDltId === id);
 
-  const { goToPage } = useNavigation();
   const token = holding?.pool.reserveToken;
-  const deficitAmount = 5;
+  const isBNT = holding?.pool.poolDltId === bntToken;
+
+  const isDisabled = toBigNumber(holding ? holding.tokenBalance : 0).isZero();
+
+  const [withdrawAmounts, setWithdrawAmounts] = useState<{
+    tkn: number;
+    bnt: number;
+    totalAmount: string;
+    baseTokenAmount: string;
+    bntAmount: string;
+  }>();
+
+  useAsyncEffect(async () => {
+    if (!holding) return;
+
+    const poolTokenBalance =
+      await ContractsApi.BancorNetworkInfo.read.underlyingToPoolToken(
+        holding.pool.poolDltId,
+        expandToken(holding.combinedTokenBalance, holding.pool.decimals)
+      );
+    const res = await fetchWithdrawalRequestOutputBreakdown(
+      holding.pool.poolDltId,
+      poolTokenBalance.toString(),
+      expandToken(holding.combinedTokenBalance, holding.pool.decimals)
+    );
+    setWithdrawAmounts(res);
+  }, [
+    holding?.pool.poolDltId,
+    holding?.combinedTokenBalance,
+    holding?.pool.decimals,
+  ]);
+
+  const deficitAmount =
+    holding && !isBNT && withdrawAmounts
+      ? shrinkToken(withdrawAmounts.baseTokenAmount ?? 0, holding.pool.decimals)
+      : undefined;
 
   return (
-    <div className="pt-100 mx-auto max-w-[1140px]">
+    <div className="pt-100 mx-auto max-w-[1140px] p-20">
       <button
         className="flex items-center gap-10 text-secondary"
         onClick={() => goToPage.portfolio()}
@@ -31,7 +75,7 @@ export const V3HoldingPage = () => {
         <IconChevronRight className="w-16 rotate-180" />
         Portfolio
       </button>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-[70px] mt-[48px]">
+      <div className="grid md:grid-cols-3 gap-[70px] mt-[48px]">
         {isLoadingHoldings || !holding || !token ? (
           'Loading'
         ) : (
@@ -63,23 +107,27 @@ export const V3HoldingPage = () => {
                 </div>
               </div>
               <hr className="my-48 border-silver dark:border-grey" />
-              <div className="flex justify-between text-secondary">
+              <div className="grid md:grid-cols-3 text-secondary">
                 <div>
                   Total Invested
-                  <div className="text-black dark:text-white mt-8">1622.2</div>
+                  <div className="text-black dark:text-white mt-8">
+                    {holding.stakedTokenBalance}
+                  </div>
                 </div>
                 <div>
                   Compunding returns
-                  <div className="text-black dark:text-white mt-8">1622.2</div>
-                  <div className="text-primary mt-8">50%</div>
+                  <div className="text-black dark:text-white mt-8">
+                    {prettifyNumber(holding.combinedTokenBalance)}
+                  </div>
+                  <div className="text-primary mt-8">???%</div>
                 </div>
                 <div>
                   Vault balance
-                  <div className="text-primary mt-8">1622.2</div>
+                  <div className="text-primary mt-8">??%</div>
                 </div>
               </div>
             </div>
-            <div>
+            <div className="w-full">
               <div className="mb-30 shadow p-30 rounded-10">
                 {token.symbol} Pool APR
                 <div>
@@ -88,32 +136,49 @@ export const V3HoldingPage = () => {
                   </div>
                   <hr className="my-30 border-silver dark:border-grey" />
                   <div className="flex items-center justify-between">
-                    <div>
+                    <div className="text-secondary">
                       Available to Deposit
                       <div className="text-black dark:text-white mt-8">
-                        1622.2
+                        {token.balance}
                       </div>
                     </div>
-                    <Button
-                      size={ButtonSize.ExtraSmall}
-                      variant={ButtonVariant.Secondary}
-                    >
-                      Deposit
-                    </Button>
+                    <DepositV3Modal
+                      pool={holding.pool}
+                      renderButton={(onClick) => (
+                        <Button
+                          onClick={() => onClick()}
+                          size={ButtonSize.ExtraSmall}
+                          variant={ButtonVariant.Secondary}
+                        >
+                          Deposit
+                        </Button>
+                      )}
+                    />
                   </div>
                 </div>
               </div>
               <div className="flex items-center justify-between p-30 shadow rounded-10">
-                <div>
+                <div className="text-secondary">
                   bn{token.symbol} Available
-                  <div className="text-black dark:text-white mt-8">1622.2</div>
+                  <div className="text-black text-20 dark:text-white mt-8">
+                    {prettifyNumber(holding.poolTokenBalance)}
+                  </div>
                 </div>
-                <Button
-                  size={ButtonSize.ExtraSmall}
-                  variant={ButtonVariant.Secondary}
-                >
-                  Withdraw
-                </Button>
+                <>
+                  <V3WithdrawModal
+                    holding={holding}
+                    isOpen={isOpen}
+                    setIsOpen={setIsOpen}
+                  />
+                  <Button
+                    size={ButtonSize.ExtraSmall}
+                    variant={ButtonVariant.Secondary}
+                    onClick={() => setIsOpen(true)}
+                    disabled={isDisabled}
+                  >
+                    Withdraw
+                  </Button>
+                </>
               </div>
             </div>
           </>
