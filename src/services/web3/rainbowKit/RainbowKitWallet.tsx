6b02df8f6938a92ed7571d68bf5f7ce407951692
Jan Langheimer <langheimer@gmail.com>
2022-08-26 16:34:22 +0200

    feature(rainbowKit): add all wallets

--- src/services/web3/rainbowKit/RainbowKitWallet.tsx
+++ src/services/web3/rainbowKit/RainbowKitWallet.tsx
@@ -1,6 +1,9 @@
 import '@rainbow-me/rainbowkit/styles.css';
-
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import {
+  RainbowKitProvider,
+  connectorsForWallets,
+  wallet,
+} from '@rainbow-me/rainbowkit';
 import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
@@ -18,10 +21,23 @@ const { chains, provider } = configureChains(
       ]
 );
 
-const { connectors } = getDefaultWallets({
-  appName: 'Bancor',
-  chains,
-});
+const connectors = connectorsForWallets([
+  {
+    groupName: 'Available',
+    wallets: [
+      wallet.metaMask({ chains }),
+      wallet.walletConnect({ chains }),
+      wallet.coinbase({ appName: 'Bancor', chains }),
+      wallet.argent({ chains }),
+      wallet.brave({ chains }),
+      wallet.imToken({ chains }),
+      wallet.trust({ chains }),
+      wallet.rainbow({ chains }),
+      wallet.ledger({ chains }),
+      wallet.steak({ chains }),
+    ],
+  },
+]);
 const wagmiClient = createClient({
   autoConnect: true,
   connectors,
