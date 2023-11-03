c0abe43defe81791c5c9c17350295cc372a30fda
Doron Zavelevsky <zavelevsky@gmail.com>
2023-11-03 03:14:32 +0000

    Revert "Revert "vote page redesign with BNT""
    This reverts commit 357a8d90852fb82878d8a70506a8b019c12bf70d.

---
+++ src/pages/vote/VoteBalances.tsx
@@ -0,0 +1,44 @@
+import { Token } from 'services/observables/tokens';
+import { prettifyNumber } from 'utils/helperFunctions';
+
+export interface VoteBalancesProps {
+  govToken: Token | undefined;
+  account: string | null | undefined;
+  stakeAmount: string | undefined;
+}
+
+export const VoteBalances: React.FC<VoteBalancesProps> = ({
+  govToken,
+  account,
+  stakeAmount,
+}) => {
+  return (
+    <div className="flex flex-col w-[220px] ml-20 border border-silver dark:border-grey rounded-10 pl-20 pr-20">
+      <div className="flex flex-col justify-evenly items-center flex-1">
+        <div className="text-secondary">Available Balance</div>
+        {!account || (govToken && govToken.balance) ? (
+          <div className="text-20 dark:text-white">
+            {govToken && govToken.balance
+              ? `${prettifyNumber(govToken.balance)} ${govToken.symbol}`
+              : '--'}
+          </div>
+        ) : (
+          <div className="loading-skeleton h-[24px] w-[140px]" />
+        )}
+      </div>
+      <hr className="border-silver dark:border-grey" />
+      <div className="flex flex-col justify-evenly items-center flex-1">
+        <div className="text-secondary">Staked Balance</div>
+        {!account || stakeAmount ? (
+          <div className="text-20 dark:text-white">
+            {stakeAmount && govToken
+              ? `${prettifyNumber(stakeAmount)} ${govToken.symbol}`
+              : '--'}
+          </div>
+        ) : (
+          <div className="loading-skeleton h-[24px] w-[140px] mb-4" />
+        )}
+      </div>
+    </div>
+  );
+};
